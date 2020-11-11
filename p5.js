b48edf64e692f477230f8c784b80cdcac1832c59
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-11-11 15:34:26 +0000

    foi

--- p5.js
+++ p5.js
@@ -15832,3 +15832,133 @@
           }
 
           function _typeof(obj) {
+            if (typeof Symbol === 'function' && _typeof2(Symbol.iterator) === 'symbol') {
+              module.exports = _typeof = function _typeof(obj) {
+                return _typeof2(obj);
+              };
+            } else {
+              module.exports = _typeof = function _typeof(obj) {
+                return obj &&
+                  typeof Symbol === 'function' &&
+                  obj.constructor === Symbol &&
+                  obj !== Symbol.prototype
+                  ? 'symbol'
+                  : _typeof2(obj);
+              };
+            }
+
+            return _typeof(obj);
+          }
+
+          module.exports = _typeof;
+        },
+        {}
+      ],
+      20: [
+        function(_dereq_, module, exports) {
+          'use strict';
+
+          exports.byteLength = byteLength;
+          exports.toByteArray = toByteArray;
+          exports.fromByteArray = fromByteArray;
+
+          var lookup = [];
+          var revLookup = [];
+          var Arr = typeof Uint8Array !== 'undefined' ? Uint8Array : Array;
+
+          var code = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+          for (var i = 0, len = code.length; i < len; ++i) {
+            lookup[i] = code[i];
+            revLookup[code.charCodeAt(i)] = i;
+          }
+
+          // Support decoding URL-safe base64 strings, as Node.js does.
+          // See: https://en.wikipedia.org/wiki/Base64#URL_applications
+          revLookup['-'.charCodeAt(0)] = 62;
+          revLookup['_'.charCodeAt(0)] = 63;
+
+          function getLens(b64) {
+            var len = b64.length;
+
+            if (len % 4 > 0) {
+              throw new Error('Invalid string. Length must be a multiple of 4');
+            }
+
+            // Trim off extra bytes after placeholder bytes are found
+            // See: https://github.com/beatgammit/base64-js/issues/42
+            var validLen = b64.indexOf('=');
+            if (validLen === -1) validLen = len;
+
+            var placeHoldersLen = validLen === len ? 0 : 4 - validLen % 4;
+
+            return [validLen, placeHoldersLen];
+          }
+
+          // base64 is 4/3 + up to two characters of the original data
+          function byteLength(b64) {
+            var lens = getLens(b64);
+            var validLen = lens[0];
+            var placeHoldersLen = lens[1];
+            return (validLen + placeHoldersLen) * 3 / 4 - placeHoldersLen;
+          }
+
+          function _byteLength(b64, validLen, placeHoldersLen) {
+            return (validLen + placeHoldersLen) * 3 / 4 - placeHoldersLen;
+          }
+
+          function toByteArray(b64) {
+            var tmp;
+            var lens = getLens(b64);
+            var validLen = lens[0];
+            var placeHoldersLen = lens[1];
+
+            var arr = new Arr(_byteLength(b64, validLen, placeHoldersLen));
+
+            var curByte = 0;
+
+            // if there are placeholders, only get up to the last complete 4 chars
+            var len = placeHoldersLen > 0 ? validLen - 4 : validLen;
+
+            var i;
+            for (i = 0; i < len; i += 4) {
+              tmp =
+                (revLookup[b64.charCodeAt(i)] << 18) |
+                (revLookup[b64.charCodeAt(i + 1)] << 12) |
+                (revLookup[b64.charCodeAt(i + 2)] << 6) |
+                revLookup[b64.charCodeAt(i + 3)];
+              arr[curByte++] = (tmp >> 16) & 0xff;
+              arr[curByte++] = (tmp >> 8) & 0xff;
+              arr[curByte++] = tmp & 0xff;
+            }
+
+            if (placeHoldersLen === 2) {
+              tmp =
+                (revLookup[b64.charCodeAt(i)] << 2) |
+                (revLookup[b64.charCodeAt(i + 1)] >> 4);
+              arr[curByte++] = tmp & 0xff;
+            }
+
+            if (placeHoldersLen === 1) {
+              tmp =
+                (revLookup[b64.charCodeAt(i)] << 10) |
+                (revLookup[b64.charCodeAt(i + 1)] << 4) |
+                (revLookup[b64.charCodeAt(i + 2)] >> 2);
+              arr[curByte++] = (tmp >> 8) & 0xff;
+              arr[curByte++] = tmp & 0xff;
+            }
+
+            return arr;
+          }
+
+          function tripletToBase64(num) {
+            return (
+              lookup[(num >> 18) & 0x3f] +
+              lookup[(num >> 12) & 0x3f] +
+              lookup[(num >> 6) & 0x3f] +
+              lookup[num & 0x3f]
+            );
+          }
+
+          function encodeChunk(uint8, start, end) {
+            var tmp;
+            var output = [];
