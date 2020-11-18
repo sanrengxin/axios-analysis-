80a258f284f27323611c32d0148e254df8fc97fc
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-11-18 19:43:44 +0000

    Fix a test.

--- p5.js
+++ p5.js
@@ -17634,3 +17634,150 @@
 
             Buffer.prototype.writeFloatLE = function writeFloatLE(value, offset, noAssert) {
               return writeFloat(this, value, offset, true, noAssert);
+            };
+
+            Buffer.prototype.writeFloatBE = function writeFloatBE(value, offset, noAssert) {
+              return writeFloat(this, value, offset, false, noAssert);
+            };
+
+            function writeDouble(buf, value, offset, littleEndian, noAssert) {
+              value = +value;
+              offset = offset >>> 0;
+              if (!noAssert) {
+                checkIEEE754(
+                  buf,
+                  value,
+                  offset,
+                  8,
+                  1.7976931348623157e308,
+                  -1.7976931348623157e308
+                );
+              }
+              ieee754.write(buf, value, offset, littleEndian, 52, 8);
+              return offset + 8;
+            }
+
+            Buffer.prototype.writeDoubleLE = function writeDoubleLE(
+              value,
+              offset,
+              noAssert
+            ) {
+              return writeDouble(this, value, offset, true, noAssert);
+            };
+
+            Buffer.prototype.writeDoubleBE = function writeDoubleBE(
+              value,
+              offset,
+              noAssert
+            ) {
+              return writeDouble(this, value, offset, false, noAssert);
+            };
+
+            // copy(targetBuffer, targetStart=0, sourceStart=0, sourceEnd=buffer.length)
+            Buffer.prototype.copy = function copy(target, targetStart, start, end) {
+              if (!Buffer.isBuffer(target))
+                throw new TypeError('argument should be a Buffer');
+              if (!start) start = 0;
+              if (!end && end !== 0) end = this.length;
+              if (targetStart >= target.length) targetStart = target.length;
+              if (!targetStart) targetStart = 0;
+              if (end > 0 && end < start) end = start;
+
+              // Copy 0 bytes; we're done
+              if (end === start) return 0;
+              if (target.length === 0 || this.length === 0) return 0;
+
+              // Fatal error conditions
+              if (targetStart < 0) {
+                throw new RangeError('targetStart out of bounds');
+              }
+              if (start < 0 || start >= this.length)
+                throw new RangeError('Index out of range');
+              if (end < 0) throw new RangeError('sourceEnd out of bounds');
+
+              // Are we oob?
+              if (end > this.length) end = this.length;
+              if (target.length - targetStart < end - start) {
+                end = target.length - targetStart + start;
+              }
+
+              var len = end - start;
+
+              if (
+                this === target &&
+                typeof Uint8Array.prototype.copyWithin === 'function'
+              ) {
+                // Use built-in when available, missing from IE11
+                this.copyWithin(targetStart, start, end);
+              } else if (this === target && start < targetStart && targetStart < end) {
+                // descending copy from end
+                for (var i = len - 1; i >= 0; --i) {
+                  target[i + targetStart] = this[i + start];
+                }
+              } else {
+                Uint8Array.prototype.set.call(
+                  target,
+                  this.subarray(start, end),
+                  targetStart
+                );
+              }
+
+              return len;
+            };
+
+            // Usage:
+            //    buffer.fill(number[, offset[, end]])
+            //    buffer.fill(buffer[, offset[, end]])
+            //    buffer.fill(string[, offset[, end]][, encoding])
+            Buffer.prototype.fill = function fill(val, start, end, encoding) {
+              // Handle string cases:
+              if (typeof val === 'string') {
+                if (typeof start === 'string') {
+                  encoding = start;
+                  start = 0;
+                  end = this.length;
+                } else if (typeof end === 'string') {
+                  encoding = end;
+                  end = this.length;
+                }
+                if (encoding !== undefined && typeof encoding !== 'string') {
+                  throw new TypeError('encoding must be a string');
+                }
+                if (typeof encoding === 'string' && !Buffer.isEncoding(encoding)) {
+                  throw new TypeError('Unknown encoding: ' + encoding);
+                }
+                if (val.length === 1) {
+                  var code = val.charCodeAt(0);
+                  if ((encoding === 'utf8' && code < 128) || encoding === 'latin1') {
+                    // Fast path: If `val` fits into a single byte, use that numeric value.
+                    val = code;
+                  }
+                }
+              } else if (typeof val === 'number') {
+                val = val & 255;
+              } else if (typeof val === 'boolean') {
+                val = Number(val);
+              }
+
+              // Invalid ranges are not set to a default, so can range check early.
+              if (start < 0 || this.length < start || this.length < end) {
+                throw new RangeError('Out of range index');
+              }
+
+              if (end <= start) {
+                return this;
+              }
+
+              start = start >>> 0;
+              end = end === undefined ? this.length : end >>> 0;
+
+              if (!val) val = 0;
+
+              var i;
+              if (typeof val === 'number') {
+                for (i = start; i < end; ++i) {
+                  this[i] = val;
+                }
+              } else {
+                var bytes = Buffer.isBuffer(val) ? val : Buffer.from(val, encoding);
+                var len = bytes.length;
