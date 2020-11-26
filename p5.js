f2caae62e4307eae25f213b01cb8b51a242ff260
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-11-26 11:00:45 +0000

    EmailtoTracScript:

--- p5.js
+++ p5.js
@@ -18474,3 +18474,143 @@
                   this.promise = new Constructor(noop);
 
                   if (!this.promise[PROMISE_ID]) {
+                    makePromise(this.promise);
+                  }
+
+                  if (isArray(input)) {
+                    this.length = input.length;
+                    this._remaining = input.length;
+
+                    this._result = new Array(this.length);
+
+                    if (this.length === 0) {
+                      fulfill(this.promise, this._result);
+                    } else {
+                      this.length = this.length || 0;
+                      this._enumerate(input);
+                      if (this._remaining === 0) {
+                        fulfill(this.promise, this._result);
+                      }
+                    }
+                  } else {
+                    reject(this.promise, validationError());
+                  }
+                }
+
+                Enumerator.prototype._enumerate = function _enumerate(input) {
+                  for (var i = 0; this._state === PENDING && i < input.length; i++) {
+                    this._eachEntry(input[i], i);
+                  }
+                };
+
+                Enumerator.prototype._eachEntry = function _eachEntry(entry, i) {
+                  var c = this._instanceConstructor;
+                  var resolve$$1 = c.resolve;
+
+                  if (resolve$$1 === resolve$1) {
+                    var _then = void 0;
+                    var error = void 0;
+                    var didError = false;
+                    try {
+                      _then = entry.then;
+                    } catch (e) {
+                      didError = true;
+                      error = e;
+                    }
+
+                    if (_then === then && entry._state !== PENDING) {
+                      this._settledAt(entry._state, i, entry._result);
+                    } else if (typeof _then !== 'function') {
+                      this._remaining--;
+                      this._result[i] = entry;
+                    } else if (c === Promise$1) {
+                      var promise = new c(noop);
+                      if (didError) {
+                        reject(promise, error);
+                      } else {
+                        handleMaybeThenable(promise, entry, _then);
+                      }
+                      this._willSettleAt(promise, i);
+                    } else {
+                      this._willSettleAt(
+                        new c(function(resolve$$1) {
+                          return resolve$$1(entry);
+                        }),
+                        i
+                      );
+                    }
+                  } else {
+                    this._willSettleAt(resolve$$1(entry), i);
+                  }
+                };
+
+                Enumerator.prototype._settledAt = function _settledAt(state, i, value) {
+                  var promise = this.promise;
+
+                  if (promise._state === PENDING) {
+                    this._remaining--;
+
+                    if (state === REJECTED) {
+                      reject(promise, value);
+                    } else {
+                      this._result[i] = value;
+                    }
+                  }
+
+                  if (this._remaining === 0) {
+                    fulfill(promise, this._result);
+                  }
+                };
+
+                Enumerator.prototype._willSettleAt = function _willSettleAt(promise, i) {
+                  var enumerator = this;
+
+                  subscribe(
+                    promise,
+                    undefined,
+                    function(value) {
+                      return enumerator._settledAt(FULFILLED, i, value);
+                    },
+                    function(reason) {
+                      return enumerator._settledAt(REJECTED, i, reason);
+                    }
+                  );
+                };
+
+                return Enumerator;
+              })();
+
+              /**
+  `Promise.all` accepts an array of promises, and returns a new promise which
+  is fulfilled with an array of fulfillment values for the passed promises, or
+  rejected with the reason of the first passed promise to be rejected. It casts all
+  elements of the passed iterable to promises as it runs this algorithm.
+
+  Example:
+
+  ```javascript
+  let promise1 = resolve(1);
+  let promise2 = resolve(2);
+  let promise3 = resolve(3);
+  let promises = [ promise1, promise2, promise3 ];
+
+  Promise.all(promises).then(function(array){
+    // The array here would be [ 1, 2, 3 ];
+  });
+  ```
+
+  If any of the `promises` given to `all` are rejected, the first promise
+  that is rejected will be given as an argument to the returned promises's
+  rejection handler. For example:
+
+  Example:
+
+  ```javascript
+  let promise1 = resolve(1);
+  let promise2 = reject(new Error("2"));
+  let promise3 = reject(new Error("3"));
+  let promises = [ promise1, promise2, promise3 ];
+
+  Promise.all(promises).then(function(array){
+    // Code here never runs because there are rejected promises!
+  }, function(error) {
