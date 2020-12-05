b4839d10ab5aa4d9fedcfd9e251c045a63f7a227
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-12-05 14:53:51 +0000

    rewrite README

--- p5.js
+++ p5.js
@@ -19325,3 +19325,138 @@
       local = Function('return this')();
     } catch (e) {
       throw new Error('polyfill failed because global object is unavailable in this environment');
+    }
+  }
+  local.fetchJsonp = fetchJsonp;
+  */
+
+            module.exports = fetchJsonp;
+          });
+        },
+        {}
+      ],
+      26: [
+        function(_dereq_, module, exports) {
+          /* FileSaver.js
+ * A saveAs() FileSaver implementation.
+ * 1.3.2
+ * 2016-06-16 18:25:19
+ *
+ * By Eli Grey, http://eligrey.com
+ * License: MIT
+ *   See https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md
+ */
+
+          /*global self */
+          /*jslint bitwise: true, indent: 4, laxbreak: true, laxcomma: true, smarttabs: true, plusplus: true */
+
+          /*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
+
+          var saveAs =
+            saveAs ||
+            (function(view) {
+              'use strict';
+              // IE <10 is explicitly unsupported
+              if (
+                typeof view === 'undefined' ||
+                (typeof navigator !== 'undefined' &&
+                  /MSIE [1-9]\./.test(navigator.userAgent))
+              ) {
+                return;
+              }
+              var doc = view.document,
+                // only get URL when necessary in case Blob.js hasn't overridden it yet
+                get_URL = function() {
+                  return view.URL || view.webkitURL || view;
+                },
+                save_link = doc.createElementNS('http://www.w3.org/1999/xhtml', 'a'),
+                can_use_save_link = 'download' in save_link,
+                click = function(node) {
+                  var event = new MouseEvent('click');
+                  node.dispatchEvent(event);
+                },
+                is_safari = /constructor/i.test(view.HTMLElement) || view.safari,
+                is_chrome_ios = /CriOS\/[\d]+/.test(navigator.userAgent),
+                throw_outside = function(ex) {
+                  (view.setImmediate || view.setTimeout)(function() {
+                    throw ex;
+                  }, 0);
+                },
+                force_saveable_type = 'application/octet-stream',
+                // the Blob API is fundamentally broken as there is no "downloadfinished" event to subscribe to
+                arbitrary_revoke_timeout = 1000 * 40, // in ms
+                revoke = function(file) {
+                  var revoker = function() {
+                    if (typeof file === 'string') {
+                      // file is an object URL
+                      get_URL().revokeObjectURL(file);
+                    } else {
+                      // file is a File
+                      file.remove();
+                    }
+                  };
+                  setTimeout(revoker, arbitrary_revoke_timeout);
+                },
+                dispatch = function(filesaver, event_types, event) {
+                  event_types = [].concat(event_types);
+                  var i = event_types.length;
+                  while (i--) {
+                    var listener = filesaver['on' + event_types[i]];
+                    if (typeof listener === 'function') {
+                      try {
+                        listener.call(filesaver, event || filesaver);
+                      } catch (ex) {
+                        throw_outside(ex);
+                      }
+                    }
+                  }
+                },
+                auto_bom = function(blob) {
+                  // prepend BOM for UTF-8 XML and text/* types (including HTML)
+                  // note: your browser will automatically convert UTF-16 U+FEFF to EF BB BF
+                  if (
+                    /^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(
+                      blob.type
+                    )
+                  ) {
+                    return new Blob([String.fromCharCode(0xfeff), blob], {
+                      type: blob.type
+                    });
+                  }
+                  return blob;
+                },
+                FileSaver = function(blob, name, no_auto_bom) {
+                  if (!no_auto_bom) {
+                    blob = auto_bom(blob);
+                  }
+                  // First try a.download, then web filesystem, then object URLs
+                  var filesaver = this,
+                    type = blob.type,
+                    force = type === force_saveable_type,
+                    object_url,
+                    dispatch_all = function() {
+                      dispatch(filesaver, 'writestart progress write writeend'.split(' '));
+                    },
+                    // on any filesys errors revert to saving with object URLs
+                    fs_error = function() {
+                      if ((is_chrome_ios || (force && is_safari)) && view.FileReader) {
+                        // Safari doesn't allow downloading of blob urls
+                        var reader = new FileReader();
+                        reader.onloadend = function() {
+                          var url = is_chrome_ios
+                            ? reader.result
+                            : reader.result.replace(
+                                /^data:[^;]*;/,
+                                'data:attachment/file;'
+                              );
+                          var popup = view.open(url, '_blank');
+                          if (!popup) view.location.href = url;
+                          url = undefined; // release reference before dispatching
+                          filesaver.readyState = filesaver.DONE;
+                          dispatch_all();
+                        };
+                        reader.readAsDataURL(blob);
+                        filesaver.readyState = filesaver.INIT;
+                        return;
+                      }
+                      // don't create more object URLs than needed
