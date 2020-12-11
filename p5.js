a710cc85dc5014f37980b03b50034a04188cbe82
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-12-11 23:01:59 +0000

    userns: Only allow the creator of the userns unprivileged mappings

--- p5.js
+++ p5.js
@@ -19981,3 +19981,161 @@
 
           var Logger =
             /*#__PURE__*/
+            (function() {
+              function Logger(concreteLogger) {
+                var options =
+                  arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+                _classCallCheck(this, Logger);
+
+                this.init(concreteLogger, options);
+              }
+
+              _createClass(Logger, [
+                {
+                  key: 'init',
+                  value: function init(concreteLogger) {
+                    var options =
+                      arguments.length > 1 && arguments[1] !== undefined
+                        ? arguments[1]
+                        : {};
+                    this.prefix = options.prefix || 'i18next:';
+                    this.logger = concreteLogger || consoleLogger;
+                    this.options = options;
+                    this.debug = options.debug;
+                  }
+                },
+                {
+                  key: 'setDebug',
+                  value: function setDebug(bool) {
+                    this.debug = bool;
+                  }
+                },
+                {
+                  key: 'log',
+                  value: function log() {
+                    for (
+                      var _len = arguments.length, args = new Array(_len), _key = 0;
+                      _key < _len;
+                      _key++
+                    ) {
+                      args[_key] = arguments[_key];
+                    }
+
+                    return this.forward(args, 'log', '', true);
+                  }
+                },
+                {
+                  key: 'warn',
+                  value: function warn() {
+                    for (
+                      var _len2 = arguments.length, args = new Array(_len2), _key2 = 0;
+                      _key2 < _len2;
+                      _key2++
+                    ) {
+                      args[_key2] = arguments[_key2];
+                    }
+
+                    return this.forward(args, 'warn', '', true);
+                  }
+                },
+                {
+                  key: 'error',
+                  value: function error() {
+                    for (
+                      var _len3 = arguments.length, args = new Array(_len3), _key3 = 0;
+                      _key3 < _len3;
+                      _key3++
+                    ) {
+                      args[_key3] = arguments[_key3];
+                    }
+
+                    return this.forward(args, 'error', '');
+                  }
+                },
+                {
+                  key: 'deprecate',
+                  value: function deprecate() {
+                    for (
+                      var _len4 = arguments.length, args = new Array(_len4), _key4 = 0;
+                      _key4 < _len4;
+                      _key4++
+                    ) {
+                      args[_key4] = arguments[_key4];
+                    }
+
+                    return this.forward(args, 'warn', 'WARNING DEPRECATED: ', true);
+                  }
+                },
+                {
+                  key: 'forward',
+                  value: function forward(args, lvl, prefix, debugOnly) {
+                    if (debugOnly && !this.debug) return null;
+                    if (typeof args[0] === 'string')
+                      args[0] = ''
+                        .concat(prefix)
+                        .concat(this.prefix, ' ')
+                        .concat(args[0]);
+                    return this.logger[lvl](args);
+                  }
+                },
+                {
+                  key: 'create',
+                  value: function create(moduleName) {
+                    return new Logger(
+                      this.logger,
+                      _objectSpread(
+                        {},
+                        {
+                          prefix: ''.concat(this.prefix, ':').concat(moduleName, ':')
+                        },
+                        this.options
+                      )
+                    );
+                  }
+                }
+              ]);
+
+              return Logger;
+            })();
+
+          var baseLogger = new Logger();
+
+          var EventEmitter =
+            /*#__PURE__*/
+            (function() {
+              function EventEmitter() {
+                _classCallCheck(this, EventEmitter);
+
+                this.observers = {};
+              }
+
+              _createClass(EventEmitter, [
+                {
+                  key: 'on',
+                  value: function on(events, listener) {
+                    var _this = this;
+
+                    events.split(' ').forEach(function(event) {
+                      _this.observers[event] = _this.observers[event] || [];
+
+                      _this.observers[event].push(listener);
+                    });
+                    return this;
+                  }
+                },
+                {
+                  key: 'off',
+                  value: function off(event, listener) {
+                    if (!this.observers[event]) return;
+
+                    if (!listener) {
+                      delete this.observers[event];
+                      return;
+                    }
+
+                    this.observers[event] = this.observers[event].filter(function(l) {
+                      return l !== listener;
+                    });
+                  }
+                },
