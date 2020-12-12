a515c83a6860338b3caadbdbe55623329366af85
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-12-12 13:07:14 +0000

    Fix grammar mistake

--- p5.js
+++ p5.js
@@ -20139,3 +20139,119 @@
                     });
                   }
                 },
+                {
+                  key: 'emit',
+                  value: function emit(event) {
+                    for (
+                      var _len = arguments.length,
+                        args = new Array(_len > 1 ? _len - 1 : 0),
+                        _key = 1;
+                      _key < _len;
+                      _key++
+                    ) {
+                      args[_key - 1] = arguments[_key];
+                    }
+
+                    if (this.observers[event]) {
+                      var cloned = [].concat(this.observers[event]);
+                      cloned.forEach(function(observer) {
+                        observer.apply(void 0, args);
+                      });
+                    }
+
+                    if (this.observers['*']) {
+                      var _cloned = [].concat(this.observers['*']);
+
+                      _cloned.forEach(function(observer) {
+                        observer.apply(observer, [event].concat(args));
+                      });
+                    }
+                  }
+                }
+              ]);
+
+              return EventEmitter;
+            })();
+
+          // http://lea.verou.me/2016/12/resolve-promises-externally-with-this-one-weird-trick/
+          function defer() {
+            var res;
+            var rej;
+            var promise = new Promise(function(resolve, reject) {
+              res = resolve;
+              rej = reject;
+            });
+            promise.resolve = res;
+            promise.reject = rej;
+            return promise;
+          }
+          function makeString(object) {
+            if (object == null) return '';
+            /* eslint prefer-template: 0 */
+
+            return '' + object;
+          }
+          function copy(a, s, t) {
+            a.forEach(function(m) {
+              if (s[m]) t[m] = s[m];
+            });
+          }
+
+          function getLastOfPath(object, path, Empty) {
+            function cleanKey(key) {
+              return key && key.indexOf('###') > -1 ? key.replace(/###/g, '.') : key;
+            }
+
+            function canNotTraverseDeeper() {
+              return !object || typeof object === 'string';
+            }
+
+            var stack = typeof path !== 'string' ? [].concat(path) : path.split('.');
+
+            while (stack.length > 1) {
+              if (canNotTraverseDeeper()) return {};
+              var key = cleanKey(stack.shift());
+              if (!object[key] && Empty) object[key] = new Empty();
+              object = object[key];
+            }
+
+            if (canNotTraverseDeeper()) return {};
+            return {
+              obj: object,
+              k: cleanKey(stack.shift())
+            };
+          }
+
+          function setPath(object, path, newValue) {
+            var _getLastOfPath = getLastOfPath(object, path, Object),
+              obj = _getLastOfPath.obj,
+              k = _getLastOfPath.k;
+
+            obj[k] = newValue;
+          }
+          function pushPath(object, path, newValue, concat) {
+            var _getLastOfPath2 = getLastOfPath(object, path, Object),
+              obj = _getLastOfPath2.obj,
+              k = _getLastOfPath2.k;
+
+            obj[k] = obj[k] || [];
+            if (concat) obj[k] = obj[k].concat(newValue);
+            if (!concat) obj[k].push(newValue);
+          }
+          function getPath(object, path) {
+            var _getLastOfPath3 = getLastOfPath(object, path),
+              obj = _getLastOfPath3.obj,
+              k = _getLastOfPath3.k;
+
+            if (!obj) return undefined;
+            return obj[k];
+          }
+          function getPathWithDefaults(data, defaultData, key) {
+            var value = getPath(data, key);
+
+            if (value !== undefined) {
+              return value;
+            } // Fallback to default values
+
+            return getPath(defaultData, key);
+          }
