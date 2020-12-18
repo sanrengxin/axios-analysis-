0217e9065493c9499ed7648a610d568d07568da3
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-12-18 22:18:10 +0000

    Update james.html

--- p5.js
+++ p5.js
@@ -21366,3 +21366,140 @@
                 'cgg',
                 'fa',
                 'id',
+                'ja',
+                'jbo',
+                'ka',
+                'kk',
+                'km',
+                'ko',
+                'ky',
+                'lo',
+                'ms',
+                'sah',
+                'su',
+                'th',
+                'tt',
+                'ug',
+                'vi',
+                'wo',
+                'zh'
+              ],
+              nr: [1],
+              fc: 3
+            },
+            {
+              lngs: ['be', 'bs', 'cnr', 'dz', 'hr', 'ru', 'sr', 'uk'],
+              nr: [1, 2, 5],
+              fc: 4
+            },
+            {
+              lngs: ['ar'],
+              nr: [0, 1, 2, 3, 11, 100],
+              fc: 5
+            },
+            {
+              lngs: ['cs', 'sk'],
+              nr: [1, 2, 5],
+              fc: 6
+            },
+            {
+              lngs: ['csb', 'pl'],
+              nr: [1, 2, 5],
+              fc: 7
+            },
+            {
+              lngs: ['cy'],
+              nr: [1, 2, 3, 8],
+              fc: 8
+            },
+            {
+              lngs: ['fr'],
+              nr: [1, 2],
+              fc: 9
+            },
+            {
+              lngs: ['ga'],
+              nr: [1, 2, 3, 7, 11],
+              fc: 10
+            },
+            {
+              lngs: ['gd'],
+              nr: [1, 2, 3, 20],
+              fc: 11
+            },
+            {
+              lngs: ['is'],
+              nr: [1, 2],
+              fc: 12
+            },
+            {
+              lngs: ['jv'],
+              nr: [0, 1],
+              fc: 13
+            },
+            {
+              lngs: ['kw'],
+              nr: [1, 2, 3, 4],
+              fc: 14
+            },
+            {
+              lngs: ['lt'],
+              nr: [1, 2, 10],
+              fc: 15
+            },
+            {
+              lngs: ['lv'],
+              nr: [1, 2, 0],
+              fc: 16
+            },
+            {
+              lngs: ['mk'],
+              nr: [1, 2],
+              fc: 17
+            },
+            {
+              lngs: ['mnk'],
+              nr: [0, 1, 2],
+              fc: 18
+            },
+            {
+              lngs: ['mt'],
+              nr: [1, 2, 11, 20],
+              fc: 19
+            },
+            {
+              lngs: ['or'],
+              nr: [2, 1],
+              fc: 2
+            },
+            {
+              lngs: ['ro'],
+              nr: [1, 2, 20],
+              fc: 20
+            },
+            {
+              lngs: ['sl'],
+              nr: [5, 1, 2, 3],
+              fc: 21
+            },
+            {
+              lngs: ['he'],
+              nr: [1, 2, 20, 21],
+              fc: 22
+            }
+          ];
+          var _rulesPluralsTypes = {
+            1: function _(n) {
+              return Number(n > 1);
+            },
+            2: function _(n) {
+              return Number(n != 1);
+            },
+            3: function _(n) {
+              return 0;
+            },
+            4: function _(n) {
+              return Number(
+                n % 10 == 1 && n % 100 != 11
+                  ? 0
+                  : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2
