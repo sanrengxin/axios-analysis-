6bfb72134b0aab9370833ec292151e06d140c3cd
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-12-24 06:31:27 +0000

    Updated readme file.

--- p5.js
+++ p5.js
@@ -22036,3 +22036,145 @@
                 }
 
                 return _this;
+              }
+
+              _createClass(Connector, [
+                {
+                  key: 'queueLoad',
+                  value: function queueLoad(languages, namespaces, options, callback) {
+                    var _this2 = this;
+
+                    // find what needs to be loaded
+                    var toLoad = [];
+                    var pending = [];
+                    var toLoadLanguages = [];
+                    var toLoadNamespaces = [];
+                    languages.forEach(function(lng) {
+                      var hasAllNamespaces = true;
+                      namespaces.forEach(function(ns) {
+                        var name = ''.concat(lng, '|').concat(ns);
+
+                        if (!options.reload && _this2.store.hasResourceBundle(lng, ns)) {
+                          _this2.state[name] = 2; // loaded
+                        } else if (_this2.state[name] < 0);
+                        else if (_this2.state[name] === 1) {
+                          if (pending.indexOf(name) < 0) pending.push(name);
+                        } else {
+                          _this2.state[name] = 1; // pending
+
+                          hasAllNamespaces = false;
+                          if (pending.indexOf(name) < 0) pending.push(name);
+                          if (toLoad.indexOf(name) < 0) toLoad.push(name);
+                          if (toLoadNamespaces.indexOf(ns) < 0) toLoadNamespaces.push(ns);
+                        }
+                      });
+                      if (!hasAllNamespaces) toLoadLanguages.push(lng);
+                    });
+
+                    if (toLoad.length || pending.length) {
+                      this.queue.push({
+                        pending: pending,
+                        loaded: {},
+                        errors: [],
+                        callback: callback
+                      });
+                    }
+
+                    return {
+                      toLoad: toLoad,
+                      pending: pending,
+                      toLoadLanguages: toLoadLanguages,
+                      toLoadNamespaces: toLoadNamespaces
+                    };
+                  }
+                },
+                {
+                  key: 'loaded',
+                  value: function loaded(name, err, data) {
+                    var _name$split = name.split('|'),
+                      _name$split2 = _slicedToArray(_name$split, 2),
+                      lng = _name$split2[0],
+                      ns = _name$split2[1];
+
+                    if (err) this.emit('failedLoading', lng, ns, err);
+
+                    if (data) {
+                      this.store.addResourceBundle(lng, ns, data);
+                    } // set loaded
+
+                    this.state[name] = err ? -1 : 2; // consolidated loading done in this run - only emit once for a loaded namespace
+
+                    var loaded = {}; // callback if ready
+
+                    this.queue.forEach(function(q) {
+                      pushPath(q.loaded, [lng], ns);
+                      remove(q.pending, name);
+                      if (err) q.errors.push(err);
+
+                      if (q.pending.length === 0 && !q.done) {
+                        // only do once per loaded -> this.emit('loaded', q.loaded);
+                        Object.keys(q.loaded).forEach(function(l) {
+                          if (!loaded[l]) loaded[l] = [];
+
+                          if (q.loaded[l].length) {
+                            q.loaded[l].forEach(function(ns) {
+                              if (loaded[l].indexOf(ns) < 0) loaded[l].push(ns);
+                            });
+                          }
+                        });
+                        /* eslint no-param-reassign: 0 */
+
+                        q.done = true;
+
+                        if (q.errors.length) {
+                          q.callback(q.errors);
+                        } else {
+                          q.callback();
+                        }
+                      }
+                    }); // emit consolidated loaded event
+
+                    this.emit('loaded', loaded); // remove done load requests
+
+                    this.queue = this.queue.filter(function(q) {
+                      return !q.done;
+                    });
+                  }
+                },
+                {
+                  key: 'read',
+                  value: function read(lng, ns, fcName) {
+                    var _this3 = this;
+
+                    var tried =
+                      arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+                    var wait =
+                      arguments.length > 4 && arguments[4] !== undefined
+                        ? arguments[4]
+                        : 250;
+                    var callback = arguments.length > 5 ? arguments[5] : undefined;
+                    if (!lng.length) return callback(null, {}); // noting to load
+
+                    return this.backend[fcName](lng, ns, function(err, data) {
+                      if (
+                        err &&
+                        data &&
+                        /* = retryFlag */
+                        tried < 5
+                      ) {
+                        setTimeout(function() {
+                          _this3.read.call(
+                            _this3,
+                            lng,
+                            ns,
+                            fcName,
+                            tried + 1,
+                            wait * 2,
+                            callback
+                          );
+                        }, wait);
+                        return;
+                      }
+
+                      callback(err, data);
+                    });
