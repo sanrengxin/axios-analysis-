a6475a17df944d2f281402028326eef31a00aaf1
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2020-12-24 18:32:19 +0000

    Update copyright.

--- p5.js
+++ p5.js
@@ -22338,3 +22338,123 @@
               nonExplicitWhitelist: false,
               load: 'all',
               // | currentOnly | languageOnly
+              preload: false,
+              // array with preload languages
+              simplifyPluralSuffix: true,
+              keySeparator: '.',
+              nsSeparator: ':',
+              pluralSeparator: '_',
+              contextSeparator: '_',
+              partialBundledLanguages: false,
+              // allow bundling certain languages that are not remotely fetched
+              saveMissing: false,
+              // enable to send missing values
+              updateMissing: false,
+              // enable to update default values if different from translated value (only useful on initial development, or when keeping code as source of truth)
+              saveMissingTo: 'fallback',
+              // 'current' || 'all'
+              saveMissingPlurals: true,
+              // will save all forms not only singular key
+              missingKeyHandler: false,
+              // function(lng, ns, key, fallbackValue) -> override if prefer on handling
+              missingInterpolationHandler: false,
+              // function(str, match)
+              postProcess: false,
+              // string or array of postProcessor names
+              postProcessPassResolved: false,
+              // pass resolved object into 'options.i18nResolved' for postprocessor
+              returnNull: true,
+              // allows null value as valid translation
+              returnEmptyString: true,
+              // allows empty string value as valid translation
+              returnObjects: false,
+              joinArrays: false,
+              // or string to join array
+              returnedObjectHandler: false,
+              // function(key, value, options) triggered if key returns object but returnObjects is set to false
+              parseMissingKeyHandler: false,
+              // function(key) parsed a key that was not found in t() before returning
+              appendNamespaceToMissingKey: false,
+              appendNamespaceToCIMode: false,
+              overloadTranslationOptionHandler: function handle(args) {
+                var ret = {};
+                if (_typeof(args[1]) === 'object') ret = args[1];
+                if (typeof args[1] === 'string') ret.defaultValue = args[1];
+                if (typeof args[2] === 'string') ret.tDescription = args[2];
+
+                if (_typeof(args[2]) === 'object' || _typeof(args[3]) === 'object') {
+                  var options = args[3] || args[2];
+                  Object.keys(options).forEach(function(key) {
+                    ret[key] = options[key];
+                  });
+                }
+
+                return ret;
+              },
+              interpolation: {
+                escapeValue: true,
+                format: function format(value, _format, lng) {
+                  return value;
+                },
+                prefix: '{{',
+                suffix: '}}',
+                formatSeparator: ',',
+                // prefixEscaped: '{{',
+                // suffixEscaped: '}}',
+                // unescapeSuffix: '',
+                unescapePrefix: '-',
+                nestingPrefix: '$t(',
+                nestingSuffix: ')',
+                // nestingPrefixEscaped: '$t(',
+                // nestingSuffixEscaped: ')',
+                // defaultVariables: undefined // object that can have values to interpolate on - extends passed in interpolation data
+                maxReplaces: 1000 // max replaces to prevent endless loop
+              }
+            };
+          }
+          /* eslint no-param-reassign: 0 */
+
+          function transformOptions(options) {
+            // create namespace object if namespace is passed in as string
+            if (typeof options.ns === 'string') options.ns = [options.ns];
+            if (typeof options.fallbackLng === 'string')
+              options.fallbackLng = [options.fallbackLng];
+            if (typeof options.fallbackNS === 'string')
+              options.fallbackNS = [options.fallbackNS]; // extend whitelist with cimode
+
+            if (options.whitelist && options.whitelist.indexOf('cimode') < 0) {
+              options.whitelist = options.whitelist.concat(['cimode']);
+            }
+
+            return options;
+          }
+
+          function noop() {}
+
+          var I18n =
+            /*#__PURE__*/
+            (function(_EventEmitter) {
+              _inherits(I18n, _EventEmitter);
+
+              function I18n() {
+                var _this;
+
+                var options =
+                  arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+                var callback = arguments.length > 1 ? arguments[1] : undefined;
+
+                _classCallCheck(this, I18n);
+
+                _this = _possibleConstructorReturn(this, _getPrototypeOf(I18n).call(this));
+                EventEmitter.call(_assertThisInitialized(_this)); // <=IE10 fix (unable to call parent constructor)
+
+                _this.options = transformOptions(options);
+                _this.services = {};
+                _this.logger = baseLogger;
+                _this.modules = {
+                  external: []
+                };
+
+                if (callback && !_this.isInitialized && !options.isClone) {
+                  // https://github.com/i18next/i18next/issues/879
+                  if (!_this.options.initImmediate) {
