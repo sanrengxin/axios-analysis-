2cba33d4e251602211e248914f98b23c2d841969
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-01-14 16:52:28 +0000

    changes for heroku

--- p5.js
+++ p5.js
@@ -24481,3 +24481,132 @@
             }
 
             // - Logical Screen Descriptor.
+            // NOTE(deanm): w/h apparently ignored by implementations, but set anyway.
+            buf[p++] = width & 0xff;
+            buf[p++] = (width >> 8) & 0xff;
+            buf[p++] = height & 0xff;
+            buf[p++] = (height >> 8) & 0xff;
+            // NOTE: Indicates 0-bpp original color resolution (unused?).
+            buf[p++] =
+              (global_palette !== null ? 0x80 : 0) | gp_num_colors_pow2; // Global Color Table Flag. // NOTE: No sort flag (unused?).
+            buf[p++] = background; // Background Color Index.
+            buf[p++] = 0; // Pixel aspect ratio (unused?).
+
+            // - Global Color Table
+            if (global_palette !== null) {
+              for (var i = 0, il = global_palette.length; i < il; ++i) {
+                var rgb = global_palette[i];
+                buf[p++] = (rgb >> 16) & 0xff;
+                buf[p++] = (rgb >> 8) & 0xff;
+                buf[p++] = rgb & 0xff;
+              }
+            }
+
+            if (loop_count !== null) {
+              // Netscape block for looping.
+              if (loop_count < 0 || loop_count > 65535)
+                throw new Error('Loop count invalid.');
+              // Extension code, label, and length.
+              buf[p++] = 0x21;
+              buf[p++] = 0xff;
+              buf[p++] = 0x0b;
+              // NETSCAPE2.0
+              buf[p++] = 0x4e;
+              buf[p++] = 0x45;
+              buf[p++] = 0x54;
+              buf[p++] = 0x53;
+              buf[p++] = 0x43;
+              buf[p++] = 0x41;
+              buf[p++] = 0x50;
+              buf[p++] = 0x45;
+              buf[p++] = 0x32;
+              buf[p++] = 0x2e;
+              buf[p++] = 0x30;
+              // Sub-block
+              buf[p++] = 0x03;
+              buf[p++] = 0x01;
+              buf[p++] = loop_count & 0xff;
+              buf[p++] = (loop_count >> 8) & 0xff;
+              buf[p++] = 0x00; // Terminator.
+            }
+
+            var ended = false;
+
+            this.addFrame = function(x, y, w, h, indexed_pixels, opts) {
+              if (ended === true) {
+                --p;
+                ended = false;
+              } // Un-end.
+
+              opts = opts === undefined ? {} : opts;
+
+              // TODO(deanm): Bounds check x, y.  Do they need to be within the virtual
+              // canvas width/height, I imagine?
+              if (x < 0 || y < 0 || x > 65535 || y > 65535) throw new Error('x/y invalid.');
+
+              if (w <= 0 || h <= 0 || w > 65535 || h > 65535)
+                throw new Error('Width/Height invalid.');
+
+              if (indexed_pixels.length < w * h)
+                throw new Error('Not enough pixels for the frame size.');
+
+              var using_local_palette = true;
+              var palette = opts.palette;
+              if (palette === undefined || palette === null) {
+                using_local_palette = false;
+                palette = global_palette;
+              }
+
+              if (palette === undefined || palette === null)
+                throw new Error('Must supply either a local or global palette.');
+
+              var num_colors = check_palette_and_num_colors(palette);
+
+              // Compute the min_code_size (power of 2), destroying num_colors.
+              var min_code_size = 0;
+              while ((num_colors >>= 1)) ++min_code_size;
+              num_colors = 1 << min_code_size; // Now we can easily get it back.
+
+              var delay = opts.delay === undefined ? 0 : opts.delay;
+
+              // From the spec:
+              //     0 -   No disposal specified. The decoder is
+              //           not required to take any action.
+              //     1 -   Do not dispose. The graphic is to be left
+              //           in place.
+              //     2 -   Restore to background color. The area used by the
+              //           graphic must be restored to the background color.
+              //     3 -   Restore to previous. The decoder is required to
+              //           restore the area overwritten by the graphic with
+              //           what was there prior to rendering the graphic.
+              //  4-7 -    To be defined.
+              // NOTE(deanm): Dispose background doesn't really work, apparently most
+              // browsers ignore the background palette index and clear to transparency.
+              var disposal = opts.disposal === undefined ? 0 : opts.disposal;
+              if (disposal < 0 || disposal > 3)
+                // 4-7 is reserved.
+                throw new Error('Disposal out of range.');
+
+              var use_transparency = false;
+              var transparent_index = 0;
+              if (opts.transparent !== undefined && opts.transparent !== null) {
+                use_transparency = true;
+                transparent_index = opts.transparent;
+                if (transparent_index < 0 || transparent_index >= num_colors)
+                  throw new Error('Transparent color index.');
+              }
+
+              if (disposal !== 0 || use_transparency || delay !== 0) {
+                // - Graphics Control Extension
+                buf[p++] = 0x21;
+                buf[p++] = 0xf9; // Extension / Label.
+                buf[p++] = 4; // Byte size.
+
+                buf[p++] = (disposal << 2) | (use_transparency === true ? 1 : 0);
+                buf[p++] = delay & 0xff;
+                buf[p++] = (delay >> 8) & 0xff;
+                buf[p++] = transparent_index; // Transparent color index.
+                buf[p++] = 0; // Block Terminator.
+              }
+
+              // - Image Descriptor
