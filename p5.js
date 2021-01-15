74d573886e18f9c6e21534582ae251519d6e7e81
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-01-15 07:11:39 +0000

    some small improvements

--- p5.js
+++ p5.js
@@ -24740,3 +24740,153 @@
 
             // Output code for the current contents of the index buffer.
             var ib_code = index_stream[0] & code_mask; // Load first input index.
+            var code_table = {}; // Key'd on our 20-bit "tuple".
+
+            emit_code(clear_code); // Spec says first code should be a clear code.
+
+            // First index already loaded, process the rest of the stream.
+            for (var i = 1, il = index_stream.length; i < il; ++i) {
+              var k = index_stream[i] & code_mask;
+              var cur_key = (ib_code << 8) | k; // (prev, k) unique tuple.
+              var cur_code = code_table[cur_key]; // buffer + k.
+
+              // Check if we have to create a new code table entry.
+              if (cur_code === undefined) {
+                // We don't have buffer + k.
+                // Emit index buffer (without k).
+                // This is an inline version of emit_code, because this is the core
+                // writing routine of the compressor (and V8 cannot inline emit_code
+                // because it is a closure here in a different context).  Additionally
+                // we can call emit_byte_to_buffer less often, because we can have
+                // 30-bits (from our 31-bit signed SMI), and we know our codes will only
+                // be 12-bits, so can safely have 18-bits there without overflow.
+                // emit_code(ib_code);
+                cur |= ib_code << cur_shift;
+                cur_shift += cur_code_size;
+                while (cur_shift >= 8) {
+                  buf[p++] = cur & 0xff;
+                  cur >>= 8;
+                  cur_shift -= 8;
+                  if (p === cur_subblock + 256) {
+                    // Finished a subblock.
+                    buf[cur_subblock] = 255;
+                    cur_subblock = p++;
+                  }
+                }
+
+                if (next_code === 4096) {
+                  // Table full, need a clear.
+                  emit_code(clear_code);
+                  next_code = eoi_code + 1;
+                  cur_code_size = min_code_size + 1;
+                  code_table = {};
+                } else {
+                  // Table not full, insert a new entry.
+                  // Increase our variable bit code sizes if necessary.  This is a bit
+                  // tricky as it is based on "timing" between the encoding and
+                  // decoder.  From the encoders perspective this should happen after
+                  // we've already emitted the index buffer and are about to create the
+                  // first table entry that would overflow our current code bit size.
+                  if (next_code >= 1 << cur_code_size) ++cur_code_size;
+                  code_table[cur_key] = next_code++; // Insert into code table.
+                }
+
+                ib_code = k; // Index buffer to single input k.
+              } else {
+                ib_code = cur_code; // Index buffer to sequence in code table.
+              }
+            }
+
+            emit_code(ib_code); // There will still be something in the index buffer.
+            emit_code(eoi_code); // End Of Information.
+
+            // Flush / finalize the sub-blocks stream to the buffer.
+            emit_bytes_to_buffer(1);
+
+            // Finish the sub-blocks, writing out any unfinished lengths and
+            // terminating with a sub-block of length 0.  If we have already started
+            // but not yet used a sub-block it can just become the terminator.
+            if (cur_subblock + 1 === p) {
+              // Started but unused.
+              buf[cur_subblock] = 0;
+            } else {
+              // Started and used, write length and additional terminator block.
+              buf[cur_subblock] = p - cur_subblock - 1;
+              buf[p++] = 0;
+            }
+            return p;
+          }
+
+          function GifReader(buf) {
+            var p = 0;
+
+            // - Header (GIF87a or GIF89a).
+            if (
+              buf[p++] !== 0x47 ||
+              buf[p++] !== 0x49 ||
+              buf[p++] !== 0x46 ||
+              buf[p++] !== 0x38 ||
+              ((buf[p++] + 1) & 0xfd) !== 0x38 ||
+              buf[p++] !== 0x61
+            ) {
+              throw new Error('Invalid GIF 87a/89a header.');
+            }
+
+            // - Logical Screen Descriptor.
+            var width = buf[p++] | (buf[p++] << 8);
+            var height = buf[p++] | (buf[p++] << 8);
+            var pf0 = buf[p++]; // <Packed Fields>.
+            var global_palette_flag = pf0 >> 7;
+            var num_global_colors_pow2 = pf0 & 0x7;
+            var num_global_colors = 1 << (num_global_colors_pow2 + 1);
+            var background = buf[p++];
+            buf[p++]; // Pixel aspect ratio (unused?).
+
+            var global_palette_offset = null;
+            var global_palette_size = null;
+
+            if (global_palette_flag) {
+              global_palette_offset = p;
+              global_palette_size = num_global_colors;
+              p += num_global_colors * 3; // Seek past palette.
+            }
+
+            var no_eof = true;
+
+            var frames = [];
+
+            var delay = 0;
+            var transparent_index = null;
+            var disposal = 0; // 0 - No disposal specified.
+            var loop_count = null;
+
+            this.width = width;
+            this.height = height;
+
+            while (no_eof && p < buf.length) {
+              switch (buf[p++]) {
+                case 0x21: // Graphics Control Extension Block
+                  switch (buf[p++]) {
+                    case 0xff: // Application specific block
+                      // Try if it's a Netscape block (with animation loop counter).
+                      if (
+                        buf[p] !== 0x0b || // 21 FF already read, check block size.
+                        // NETSCAPE2.0
+                        (buf[p + 1] == 0x4e &&
+                          buf[p + 2] == 0x45 &&
+                          buf[p + 3] == 0x54 &&
+                          buf[p + 4] == 0x53 &&
+                          buf[p + 5] == 0x43 &&
+                          buf[p + 6] == 0x41 &&
+                          buf[p + 7] == 0x50 &&
+                          buf[p + 8] == 0x45 &&
+                          buf[p + 9] == 0x32 &&
+                          buf[p + 10] == 0x2e &&
+                          buf[p + 11] == 0x30 &&
+                          // Sub-block
+                          buf[p + 12] == 0x03 &&
+                          buf[p + 13] == 0x01 &&
+                          buf[p + 16] == 0)
+                      ) {
+                        p += 14;
+                        loop_count = buf[p++] | (buf[p++] << 8);
