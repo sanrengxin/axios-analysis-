37adef2942d66272e262055c73c7fce761a86d81
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-02-21 15:38:01 +0000

    Update messages

--- p5.js
+++ p5.js
@@ -27462,3 +27462,124 @@
                * @exports opentype.Coverage
                * @class
                * @param {opentype.Table}
+               * @constructor
+               * @extends opentype.Table
+               */
+              function Coverage(coverageTable) {
+                if (coverageTable.format === 1) {
+                  Table.call(
+                    this,
+                    'coverageTable',
+                    [{ name: 'coverageFormat', type: 'USHORT', value: 1 }].concat(
+                      ushortList('glyph', coverageTable.glyphs)
+                    )
+                  );
+                } else {
+                  check.assert(false, "Can't create coverage table format 2 yet.");
+                }
+              }
+              Coverage.prototype = Object.create(Table.prototype);
+              Coverage.prototype.constructor = Coverage;
+
+              function ScriptList(scriptListTable) {
+                Table.call(
+                  this,
+                  'scriptListTable',
+                  recordList('scriptRecord', scriptListTable, function(scriptRecord, i) {
+                    var script = scriptRecord.script;
+                    var defaultLangSys = script.defaultLangSys;
+                    check.assert(
+                      !!defaultLangSys,
+                      'Unable to write GSUB: script ' +
+                        scriptRecord.tag +
+                        ' has no default language system.'
+                    );
+                    return [
+                      { name: 'scriptTag' + i, type: 'TAG', value: scriptRecord.tag },
+                      {
+                        name: 'script' + i,
+                        type: 'TABLE',
+                        value: new Table(
+                          'scriptTable',
+                          [
+                            {
+                              name: 'defaultLangSys',
+                              type: 'TABLE',
+                              value: new Table(
+                                'defaultLangSys',
+                                [
+                                  { name: 'lookupOrder', type: 'USHORT', value: 0 },
+                                  {
+                                    name: 'reqFeatureIndex',
+                                    type: 'USHORT',
+                                    value: defaultLangSys.reqFeatureIndex
+                                  }
+                                ].concat(
+                                  ushortList('featureIndex', defaultLangSys.featureIndexes)
+                                )
+                              )
+                            }
+                          ].concat(
+                            recordList('langSys', script.langSysRecords, function(
+                              langSysRecord,
+                              i
+                            ) {
+                              var langSys = langSysRecord.langSys;
+                              return [
+                                {
+                                  name: 'langSysTag' + i,
+                                  type: 'TAG',
+                                  value: langSysRecord.tag
+                                },
+                                {
+                                  name: 'langSys' + i,
+                                  type: 'TABLE',
+                                  value: new Table(
+                                    'langSys',
+                                    [
+                                      { name: 'lookupOrder', type: 'USHORT', value: 0 },
+                                      {
+                                        name: 'reqFeatureIndex',
+                                        type: 'USHORT',
+                                        value: langSys.reqFeatureIndex
+                                      }
+                                    ].concat(
+                                      ushortList('featureIndex', langSys.featureIndexes)
+                                    )
+                                  )
+                                }
+                              ];
+                            })
+                          )
+                        )
+                      }
+                    ];
+                  })
+                );
+              }
+              ScriptList.prototype = Object.create(Table.prototype);
+              ScriptList.prototype.constructor = ScriptList;
+
+              /**
+               * @exports opentype.FeatureList
+               * @class
+               * @param {opentype.Table}
+               * @constructor
+               * @extends opentype.Table
+               */
+              function FeatureList(featureListTable) {
+                Table.call(
+                  this,
+                  'featureListTable',
+                  recordList('featureRecord', featureListTable, function(featureRecord, i) {
+                    var feature = featureRecord.feature;
+                    return [
+                      { name: 'featureTag' + i, type: 'TAG', value: featureRecord.tag },
+                      {
+                        name: 'feature' + i,
+                        type: 'TABLE',
+                        value: new Table(
+                          'featureTable',
+                          [
+                            {
+                              name: 'featureParams',
