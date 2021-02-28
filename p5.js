701cf0329eb50538fda63085c59f6848b7c8d817
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-02-28 16:20:45 +0000

    Remove an obsolete comment.

--- p5.js
+++ p5.js
@@ -28368,3 +28368,123 @@
                 var groupCount;
                 cmap.groupCount = groupCount = p.parseULong();
                 cmap.glyphIndexMap = {};
+
+                for (var i = 0; i < groupCount; i += 1) {
+                  var startCharCode = p.parseULong();
+                  var endCharCode = p.parseULong();
+                  var startGlyphId = p.parseULong();
+
+                  for (var c = startCharCode; c <= endCharCode; c += 1) {
+                    cmap.glyphIndexMap[c] = startGlyphId;
+                    startGlyphId++;
+                  }
+                }
+              }
+
+              function parseCmapTableFormat4(cmap, p, data, start, offset) {
+                // Length in bytes of the sub-tables.
+                cmap.length = p.parseUShort();
+                cmap.language = p.parseUShort();
+
+                // segCount is stored x 2.
+                var segCount;
+                cmap.segCount = segCount = p.parseUShort() >> 1;
+
+                // Skip searchRange, entrySelector, rangeShift.
+                p.skip('uShort', 3);
+
+                // The "unrolled" mapping from character codes to glyph indices.
+                cmap.glyphIndexMap = {};
+                var endCountParser = new parse.Parser(data, start + offset + 14);
+                var startCountParser = new parse.Parser(
+                  data,
+                  start + offset + 16 + segCount * 2
+                );
+                var idDeltaParser = new parse.Parser(
+                  data,
+                  start + offset + 16 + segCount * 4
+                );
+                var idRangeOffsetParser = new parse.Parser(
+                  data,
+                  start + offset + 16 + segCount * 6
+                );
+                var glyphIndexOffset = start + offset + 16 + segCount * 8;
+                for (var i = 0; i < segCount - 1; i += 1) {
+                  var glyphIndex = void 0;
+                  var endCount = endCountParser.parseUShort();
+                  var startCount = startCountParser.parseUShort();
+                  var idDelta = idDeltaParser.parseShort();
+                  var idRangeOffset = idRangeOffsetParser.parseUShort();
+                  for (var c = startCount; c <= endCount; c += 1) {
+                    if (idRangeOffset !== 0) {
+                      // The idRangeOffset is relative to the current position in the idRangeOffset array.
+                      // Take the current offset in the idRangeOffset array.
+                      glyphIndexOffset =
+                        idRangeOffsetParser.offset + idRangeOffsetParser.relativeOffset - 2;
+
+                      // Add the value of the idRangeOffset, which will move us into the glyphIndex array.
+                      glyphIndexOffset += idRangeOffset;
+
+                      // Then add the character index of the current segment, multiplied by 2 for USHORTs.
+                      glyphIndexOffset += (c - startCount) * 2;
+                      glyphIndex = parse.getUShort(data, glyphIndexOffset);
+                      if (glyphIndex !== 0) {
+                        glyphIndex = (glyphIndex + idDelta) & 0xffff;
+                      }
+                    } else {
+                      glyphIndex = (c + idDelta) & 0xffff;
+                    }
+
+                    cmap.glyphIndexMap[c] = glyphIndex;
+                  }
+                }
+              }
+
+              // Parse the `cmap` table. This table stores the mappings from characters to glyphs.
+              // There are many available formats, but we only support the Windows format 4 and 12.
+              // This function returns a `CmapEncoding` object or null if no supported format could be found.
+              function parseCmapTable(data, start) {
+                var cmap = {};
+                cmap.version = parse.getUShort(data, start);
+                check.argument(cmap.version === 0, 'cmap table version should be 0.');
+
+                // The cmap table can contain many sub-tables, each with their own format.
+                // We're only interested in a "platform 0" (Unicode format) and "platform 3" (Windows format) table.
+                cmap.numTables = parse.getUShort(data, start + 2);
+                var offset = -1;
+                for (var i = cmap.numTables - 1; i >= 0; i -= 1) {
+                  var platformId = parse.getUShort(data, start + 4 + i * 8);
+                  var encodingId = parse.getUShort(data, start + 4 + i * 8 + 2);
+                  if (
+                    (platformId === 3 &&
+                      (encodingId === 0 || encodingId === 1 || encodingId === 10)) ||
+                    (platformId === 0 &&
+                      (encodingId === 0 ||
+                        encodingId === 1 ||
+                        encodingId === 2 ||
+                        encodingId === 3 ||
+                        encodingId === 4))
+                  ) {
+                    offset = parse.getULong(data, start + 4 + i * 8 + 4);
+                    break;
+                  }
+                }
+
+                if (offset === -1) {
+                  // There is no cmap table in the font that we support.
+                  throw new Error('No valid cmap sub-tables found.');
+                }
+
+                var p = new parse.Parser(data, start + offset);
+                cmap.format = p.parseUShort();
+
+                if (cmap.format === 12) {
+                  parseCmapTableFormat12(cmap, p);
+                } else if (cmap.format === 4) {
+                  parseCmapTableFormat4(cmap, p, data, start, offset);
+                } else {
+                  throw new Error(
+                    'Only format 4 and 12 cmap tables are supported (found format ' +
+                      cmap.format +
+                      ').'
+                  );
