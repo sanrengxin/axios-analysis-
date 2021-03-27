5244fb07c36a44da34df656b1022082f54bf75d0
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-03-27 14:27:27 +0000

    Style refactoring

--- p5.js
+++ p5.js
@@ -32324,3 +32324,134 @@
                 143: 'iu',
                 144: 'gd',
                 145: 'gv',
+                146: 'ga',
+                147: 'to',
+                148: 'el-polyton',
+                149: 'kl',
+                150: 'az',
+                151: 'nn'
+              };
+
+              // MacOS language ID → MacOS script ID
+              //
+              // Note that the script ID is not sufficient to determine what encoding
+              // to use in TrueType files. For some languages, MacOS used a modification
+              // of a mainstream script. For example, an Icelandic name would be stored
+              // with smRoman in the TrueType naming table, but the actual encoding
+              // is a special Icelandic version of the normal Macintosh Roman encoding.
+              // As another example, Inuktitut uses an 8-bit encoding for Canadian Aboriginal
+              // Syllables but MacOS had run out of available script codes, so this was
+              // done as a (pretty radical) "modification" of Ethiopic.
+              //
+              // http://unicode.org/Public/MAPPINGS/VENDORS/APPLE/Readme.txt
+              var macLanguageToScript = {
+                0: 0, // langEnglish → smRoman
+                1: 0, // langFrench → smRoman
+                2: 0, // langGerman → smRoman
+                3: 0, // langItalian → smRoman
+                4: 0, // langDutch → smRoman
+                5: 0, // langSwedish → smRoman
+                6: 0, // langSpanish → smRoman
+                7: 0, // langDanish → smRoman
+                8: 0, // langPortuguese → smRoman
+                9: 0, // langNorwegian → smRoman
+                10: 5, // langHebrew → smHebrew
+                11: 1, // langJapanese → smJapanese
+                12: 4, // langArabic → smArabic
+                13: 0, // langFinnish → smRoman
+                14: 6, // langGreek → smGreek
+                15: 0, // langIcelandic → smRoman (modified)
+                16: 0, // langMaltese → smRoman
+                17: 0, // langTurkish → smRoman (modified)
+                18: 0, // langCroatian → smRoman (modified)
+                19: 2, // langTradChinese → smTradChinese
+                20: 4, // langUrdu → smArabic
+                21: 9, // langHindi → smDevanagari
+                22: 21, // langThai → smThai
+                23: 3, // langKorean → smKorean
+                24: 29, // langLithuanian → smCentralEuroRoman
+                25: 29, // langPolish → smCentralEuroRoman
+                26: 29, // langHungarian → smCentralEuroRoman
+                27: 29, // langEstonian → smCentralEuroRoman
+                28: 29, // langLatvian → smCentralEuroRoman
+                29: 0, // langSami → smRoman
+                30: 0, // langFaroese → smRoman (modified)
+                31: 4, // langFarsi → smArabic (modified)
+                32: 7, // langRussian → smCyrillic
+                33: 25, // langSimpChinese → smSimpChinese
+                34: 0, // langFlemish → smRoman
+                35: 0, // langIrishGaelic → smRoman (modified)
+                36: 0, // langAlbanian → smRoman
+                37: 0, // langRomanian → smRoman (modified)
+                38: 29, // langCzech → smCentralEuroRoman
+                39: 29, // langSlovak → smCentralEuroRoman
+                40: 0, // langSlovenian → smRoman (modified)
+                41: 5, // langYiddish → smHebrew
+                42: 7, // langSerbian → smCyrillic
+                43: 7, // langMacedonian → smCyrillic
+                44: 7, // langBulgarian → smCyrillic
+                45: 7, // langUkrainian → smCyrillic (modified)
+                46: 7, // langByelorussian → smCyrillic
+                47: 7, // langUzbek → smCyrillic
+                48: 7, // langKazakh → smCyrillic
+                49: 7, // langAzerbaijani → smCyrillic
+                50: 4, // langAzerbaijanAr → smArabic
+                51: 24, // langArmenian → smArmenian
+                52: 23, // langGeorgian → smGeorgian
+                53: 7, // langMoldavian → smCyrillic
+                54: 7, // langKirghiz → smCyrillic
+                55: 7, // langTajiki → smCyrillic
+                56: 7, // langTurkmen → smCyrillic
+                57: 27, // langMongolian → smMongolian
+                58: 7, // langMongolianCyr → smCyrillic
+                59: 4, // langPashto → smArabic
+                60: 4, // langKurdish → smArabic
+                61: 4, // langKashmiri → smArabic
+                62: 4, // langSindhi → smArabic
+                63: 26, // langTibetan → smTibetan
+                64: 9, // langNepali → smDevanagari
+                65: 9, // langSanskrit → smDevanagari
+                66: 9, // langMarathi → smDevanagari
+                67: 13, // langBengali → smBengali
+                68: 13, // langAssamese → smBengali
+                69: 11, // langGujarati → smGujarati
+                70: 10, // langPunjabi → smGurmukhi
+                71: 12, // langOriya → smOriya
+                72: 17, // langMalayalam → smMalayalam
+                73: 16, // langKannada → smKannada
+                74: 14, // langTamil → smTamil
+                75: 15, // langTelugu → smTelugu
+                76: 18, // langSinhalese → smSinhalese
+                77: 19, // langBurmese → smBurmese
+                78: 20, // langKhmer → smKhmer
+                79: 22, // langLao → smLao
+                80: 30, // langVietnamese → smVietnamese
+                81: 0, // langIndonesian → smRoman
+                82: 0, // langTagalog → smRoman
+                83: 0, // langMalayRoman → smRoman
+                84: 4, // langMalayArabic → smArabic
+                85: 28, // langAmharic → smEthiopic
+                86: 28, // langTigrinya → smEthiopic
+                87: 28, // langOromo → smEthiopic
+                88: 0, // langSomali → smRoman
+                89: 0, // langSwahili → smRoman
+                90: 0, // langKinyarwanda → smRoman
+                91: 0, // langRundi → smRoman
+                92: 0, // langNyanja → smRoman
+                93: 0, // langMalagasy → smRoman
+                94: 0, // langEsperanto → smRoman
+                128: 0, // langWelsh → smRoman (modified)
+                129: 0, // langBasque → smRoman
+                130: 0, // langCatalan → smRoman
+                131: 0, // langLatin → smRoman
+                132: 0, // langQuechua → smRoman
+                133: 0, // langGuarani → smRoman
+                134: 0, // langAymara → smRoman
+                135: 7, // langTatar → smCyrillic
+                136: 4, // langUighur → smArabic
+                137: 26, // langDzongkha → smTibetan
+                138: 0, // langJavaneseRom → smRoman
+                139: 0, // langSundaneseRom → smRoman
+                140: 0, // langGalician → smRoman
+                141: 0, // langAfrikaans → smRoman
+                142: 0, // langBreton → smRoman (modified)
