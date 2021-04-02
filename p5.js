1c854eb178fa46547a1e36a0c8e5c014fa12feef
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-04-02 12:18:24 +0000

    modify package

--- p5.js
+++ p5.js
@@ -32720,3 +32720,131 @@
                 }
 
                 return undefined;
+              }
+
+              var utf16 = 'utf-16';
+
+              // MacOS script ID → encoding. This table stores the default case,
+              // which can be overridden by macLanguageEncodings.
+              var macScriptEncodings = {
+                0: 'macintosh', // smRoman
+                1: 'x-mac-japanese', // smJapanese
+                2: 'x-mac-chinesetrad', // smTradChinese
+                3: 'x-mac-korean', // smKorean
+                6: 'x-mac-greek', // smGreek
+                7: 'x-mac-cyrillic', // smCyrillic
+                9: 'x-mac-devanagai', // smDevanagari
+                10: 'x-mac-gurmukhi', // smGurmukhi
+                11: 'x-mac-gujarati', // smGujarati
+                12: 'x-mac-oriya', // smOriya
+                13: 'x-mac-bengali', // smBengali
+                14: 'x-mac-tamil', // smTamil
+                15: 'x-mac-telugu', // smTelugu
+                16: 'x-mac-kannada', // smKannada
+                17: 'x-mac-malayalam', // smMalayalam
+                18: 'x-mac-sinhalese', // smSinhalese
+                19: 'x-mac-burmese', // smBurmese
+                20: 'x-mac-khmer', // smKhmer
+                21: 'x-mac-thai', // smThai
+                22: 'x-mac-lao', // smLao
+                23: 'x-mac-georgian', // smGeorgian
+                24: 'x-mac-armenian', // smArmenian
+                25: 'x-mac-chinesesimp', // smSimpChinese
+                26: 'x-mac-tibetan', // smTibetan
+                27: 'x-mac-mongolian', // smMongolian
+                28: 'x-mac-ethiopic', // smEthiopic
+                29: 'x-mac-ce', // smCentralEuroRoman
+                30: 'x-mac-vietnamese', // smVietnamese
+                31: 'x-mac-extarabic' // smExtArabic
+              };
+
+              // MacOS language ID → encoding. This table stores the exceptional
+              // cases, which override macScriptEncodings. For writing MacOS naming
+              // tables, we need to emit a MacOS script ID. Therefore, we cannot
+              // merge macScriptEncodings into macLanguageEncodings.
+              //
+              // http://unicode.org/Public/MAPPINGS/VENDORS/APPLE/Readme.txt
+              var macLanguageEncodings = {
+                15: 'x-mac-icelandic', // langIcelandic
+                17: 'x-mac-turkish', // langTurkish
+                18: 'x-mac-croatian', // langCroatian
+                24: 'x-mac-ce', // langLithuanian
+                25: 'x-mac-ce', // langPolish
+                26: 'x-mac-ce', // langHungarian
+                27: 'x-mac-ce', // langEstonian
+                28: 'x-mac-ce', // langLatvian
+                30: 'x-mac-icelandic', // langFaroese
+                37: 'x-mac-romanian', // langRomanian
+                38: 'x-mac-ce', // langCzech
+                39: 'x-mac-ce', // langSlovak
+                40: 'x-mac-ce', // langSlovenian
+                143: 'x-mac-inuit', // langInuktitut
+                146: 'x-mac-gaelic' // langIrishGaelicScript
+              };
+
+              function getEncoding(platformID, encodingID, languageID) {
+                switch (platformID) {
+                  case 0: // Unicode
+                    return utf16;
+
+                  case 1: // Apple Macintosh
+                    return (
+                      macLanguageEncodings[languageID] || macScriptEncodings[encodingID]
+                    );
+
+                  case 3: // Microsoft Windows
+                    if (encodingID === 1 || encodingID === 10) {
+                      return utf16;
+                    }
+
+                    break;
+                }
+
+                return undefined;
+              }
+
+              // Parse the naming `name` table.
+              // FIXME: Format 1 additional fields are not supported yet.
+              // ltag is the content of the `ltag' table, such as ['en', 'zh-Hans', 'de-CH-1904'].
+              function parseNameTable(data, start, ltag) {
+                var name = {};
+                var p = new parse.Parser(data, start);
+                var format = p.parseUShort();
+                var count = p.parseUShort();
+                var stringOffset = p.offset + p.parseUShort();
+                for (var i = 0; i < count; i++) {
+                  var platformID = p.parseUShort();
+                  var encodingID = p.parseUShort();
+                  var languageID = p.parseUShort();
+                  var nameID = p.parseUShort();
+                  var property = nameTableNames[nameID] || nameID;
+                  var byteLength = p.parseUShort();
+                  var offset = p.parseUShort();
+                  var language = getLanguageCode(platformID, languageID, ltag);
+                  var encoding = getEncoding(platformID, encodingID, languageID);
+                  if (encoding !== undefined && language !== undefined) {
+                    var text = void 0;
+                    if (encoding === utf16) {
+                      text = decode.UTF16(data, stringOffset + offset, byteLength);
+                    } else {
+                      text = decode.MACSTRING(
+                        data,
+                        stringOffset + offset,
+                        byteLength,
+                        encoding
+                      );
+                    }
+
+                    if (text) {
+                      var translations = name[property];
+                      if (translations === undefined) {
+                        translations = name[property] = {};
+                      }
+
+                      translations[language] = text;
+                    }
+                  }
+                }
+
+                var langTagCount = 0;
+                if (format === 1) {
