fb037bc11f9038d36af6c7936824b4e430dc8976
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-04-05 18:01:28 +0000

    adjust gitignore

--- p5.js
+++ p5.js
@@ -33379,3 +33379,143 @@
               var subtableParsers = new Array(9); // subtableParsers[0] is unused
 
               // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#SS
+              subtableParsers[1] = function parseLookup1() {
+                var start = this.offset + this.relativeOffset;
+                var substFormat = this.parseUShort();
+                if (substFormat === 1) {
+                  return {
+                    substFormat: 1,
+                    coverage: this.parsePointer(Parser.coverage),
+                    deltaGlyphId: this.parseUShort()
+                  };
+                } else if (substFormat === 2) {
+                  return {
+                    substFormat: 2,
+                    coverage: this.parsePointer(Parser.coverage),
+                    substitute: this.parseOffset16List()
+                  };
+                }
+                check.assert(
+                  false,
+                  '0x' + start.toString(16) + ': lookup type 1 format must be 1 or 2.'
+                );
+              };
+
+              // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#MS
+              subtableParsers[2] = function parseLookup2() {
+                var substFormat = this.parseUShort();
+                check.argument(
+                  substFormat === 1,
+                  'GSUB Multiple Substitution Subtable identifier-format must be 1'
+                );
+                return {
+                  substFormat: substFormat,
+                  coverage: this.parsePointer(Parser.coverage),
+                  sequences: this.parseListOfLists()
+                };
+              };
+
+              // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#AS
+              subtableParsers[3] = function parseLookup3() {
+                var substFormat = this.parseUShort();
+                check.argument(
+                  substFormat === 1,
+                  'GSUB Alternate Substitution Subtable identifier-format must be 1'
+                );
+                return {
+                  substFormat: substFormat,
+                  coverage: this.parsePointer(Parser.coverage),
+                  alternateSets: this.parseListOfLists()
+                };
+              };
+
+              // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#LS
+              subtableParsers[4] = function parseLookup4() {
+                var substFormat = this.parseUShort();
+                check.argument(
+                  substFormat === 1,
+                  'GSUB ligature table identifier-format must be 1'
+                );
+                return {
+                  substFormat: substFormat,
+                  coverage: this.parsePointer(Parser.coverage),
+                  ligatureSets: this.parseListOfLists(function() {
+                    return {
+                      ligGlyph: this.parseUShort(),
+                      components: this.parseUShortList(this.parseUShort() - 1)
+                    };
+                  })
+                };
+              };
+
+              var lookupRecordDesc = {
+                sequenceIndex: Parser.uShort,
+                lookupListIndex: Parser.uShort
+              };
+
+              // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#CSF
+              subtableParsers[5] = function parseLookup5() {
+                var start = this.offset + this.relativeOffset;
+                var substFormat = this.parseUShort();
+
+                if (substFormat === 1) {
+                  return {
+                    substFormat: substFormat,
+                    coverage: this.parsePointer(Parser.coverage),
+                    ruleSets: this.parseListOfLists(function() {
+                      var glyphCount = this.parseUShort();
+                      var substCount = this.parseUShort();
+                      return {
+                        input: this.parseUShortList(glyphCount - 1),
+                        lookupRecords: this.parseRecordList(substCount, lookupRecordDesc)
+                      };
+                    })
+                  };
+                } else if (substFormat === 2) {
+                  return {
+                    substFormat: substFormat,
+                    coverage: this.parsePointer(Parser.coverage),
+                    classDef: this.parsePointer(Parser.classDef),
+                    classSets: this.parseListOfLists(function() {
+                      var glyphCount = this.parseUShort();
+                      var substCount = this.parseUShort();
+                      return {
+                        classes: this.parseUShortList(glyphCount - 1),
+                        lookupRecords: this.parseRecordList(substCount, lookupRecordDesc)
+                      };
+                    })
+                  };
+                } else if (substFormat === 3) {
+                  var glyphCount = this.parseUShort();
+                  var substCount = this.parseUShort();
+                  return {
+                    substFormat: substFormat,
+                    coverages: this.parseList(glyphCount, Parser.pointer(Parser.coverage)),
+                    lookupRecords: this.parseRecordList(substCount, lookupRecordDesc)
+                  };
+                }
+                check.assert(
+                  false,
+                  '0x' + start.toString(16) + ': lookup type 5 format must be 1, 2 or 3.'
+                );
+              };
+
+              // https://www.microsoft.com/typography/OTSPEC/GSUB.htm#CC
+              subtableParsers[6] = function parseLookup6() {
+                var start = this.offset + this.relativeOffset;
+                var substFormat = this.parseUShort();
+                if (substFormat === 1) {
+                  return {
+                    substFormat: 1,
+                    coverage: this.parsePointer(Parser.coverage),
+                    chainRuleSets: this.parseListOfLists(function() {
+                      return {
+                        backtrack: this.parseUShortList(),
+                        input: this.parseUShortList(this.parseShort() - 1),
+                        lookahead: this.parseUShortList(),
+                        lookupRecords: this.parseRecordList(lookupRecordDesc)
+                      };
+                    })
+                  };
+                } else if (substFormat === 2) {
+                  return {
