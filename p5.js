d92b07a43f6ed4efbec8990ec3e9cc71447ea60e
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-04-14 06:31:32 +0000

    fix select

--- p5.js
+++ p5.js
@@ -33792,3 +33792,151 @@
               }
 
               var meta = { parse: parseMetaTable, make: makeMetaTable };
+
+              // The `sfnt` wrapper provides organization for the tables in the font.
+
+              function log2(v) {
+                return (Math.log(v) / Math.log(2)) | 0;
+              }
+
+              function computeCheckSum(bytes) {
+                while (bytes.length % 4 !== 0) {
+                  bytes.push(0);
+                }
+
+                var sum = 0;
+                for (var i = 0; i < bytes.length; i += 4) {
+                  sum +=
+                    (bytes[i] << 24) +
+                    (bytes[i + 1] << 16) +
+                    (bytes[i + 2] << 8) +
+                    bytes[i + 3];
+                }
+
+                sum %= Math.pow(2, 32);
+                return sum;
+              }
+
+              function makeTableRecord(tag, checkSum, offset, length) {
+                return new table.Record('Table Record', [
+                  { name: 'tag', type: 'TAG', value: tag !== undefined ? tag : '' },
+                  {
+                    name: 'checkSum',
+                    type: 'ULONG',
+                    value: checkSum !== undefined ? checkSum : 0
+                  },
+                  {
+                    name: 'offset',
+                    type: 'ULONG',
+                    value: offset !== undefined ? offset : 0
+                  },
+                  {
+                    name: 'length',
+                    type: 'ULONG',
+                    value: length !== undefined ? length : 0
+                  }
+                ]);
+              }
+
+              function makeSfntTable(tables) {
+                var sfnt = new table.Table('sfnt', [
+                  { name: 'version', type: 'TAG', value: 'OTTO' },
+                  { name: 'numTables', type: 'USHORT', value: 0 },
+                  { name: 'searchRange', type: 'USHORT', value: 0 },
+                  { name: 'entrySelector', type: 'USHORT', value: 0 },
+                  { name: 'rangeShift', type: 'USHORT', value: 0 }
+                ]);
+                sfnt.tables = tables;
+                sfnt.numTables = tables.length;
+                var highestPowerOf2 = Math.pow(2, log2(sfnt.numTables));
+                sfnt.searchRange = 16 * highestPowerOf2;
+                sfnt.entrySelector = log2(highestPowerOf2);
+                sfnt.rangeShift = sfnt.numTables * 16 - sfnt.searchRange;
+
+                var recordFields = [];
+                var tableFields = [];
+
+                var offset = sfnt.sizeOf() + makeTableRecord().sizeOf() * sfnt.numTables;
+                while (offset % 4 !== 0) {
+                  offset += 1;
+                  tableFields.push({ name: 'padding', type: 'BYTE', value: 0 });
+                }
+
+                for (var i = 0; i < tables.length; i += 1) {
+                  var t = tables[i];
+                  check.argument(
+                    t.tableName.length === 4,
+                    'Table name' + t.tableName + ' is invalid.'
+                  );
+                  var tableLength = t.sizeOf();
+                  var tableRecord = makeTableRecord(
+                    t.tableName,
+                    computeCheckSum(t.encode()),
+                    offset,
+                    tableLength
+                  );
+                  recordFields.push({
+                    name: tableRecord.tag + ' Table Record',
+                    type: 'RECORD',
+                    value: tableRecord
+                  });
+                  tableFields.push({
+                    name: t.tableName + ' table',
+                    type: 'RECORD',
+                    value: t
+                  });
+                  offset += tableLength;
+                  check.argument(
+                    !isNaN(offset),
+                    'Something went wrong calculating the offset.'
+                  );
+                  while (offset % 4 !== 0) {
+                    offset += 1;
+                    tableFields.push({ name: 'padding', type: 'BYTE', value: 0 });
+                  }
+                }
+
+                // Table records need to be sorted alphabetically.
+                recordFields.sort(function(r1, r2) {
+                  if (r1.value.tag > r2.value.tag) {
+                    return 1;
+                  } else {
+                    return -1;
+                  }
+                });
+
+                sfnt.fields = sfnt.fields.concat(recordFields);
+                sfnt.fields = sfnt.fields.concat(tableFields);
+                return sfnt;
+              }
+
+              // Get the metrics for a character. If the string has more than one character
+              // this function returns metrics for the first available character.
+              // You can provide optional fallback metrics if no characters are available.
+              function metricsForChar(font, chars, notFoundMetrics) {
+                for (var i = 0; i < chars.length; i += 1) {
+                  var glyphIndex = font.charToGlyphIndex(chars[i]);
+                  if (glyphIndex > 0) {
+                    var glyph = font.glyphs.get(glyphIndex);
+                    return glyph.getMetrics();
+                  }
+                }
+
+                return notFoundMetrics;
+              }
+
+              function average(vs) {
+                var sum = 0;
+                for (var i = 0; i < vs.length; i += 1) {
+                  sum += vs[i];
+                }
+
+                return sum / vs.length;
+              }
+
+              // Convert the font object to a SFNT data structure.
+              // This structure contains all the necessary tables and metadata to create a binary OTF file.
+              function fontToSfntTable(font) {
+                var xMins = [];
+                var yMins = [];
+                var xMaxs = [];
