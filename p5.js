6d61c6b1f17b0e3e4db029dde1f9f3d9060db64c
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-06-19 07:05:26 +0000

    README update

--- p5.js
+++ p5.js
@@ -40574,3 +40574,132 @@
             } catch (e) {
               try {
                 // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+                return cachedSetTimeout.call(null, fun, 0);
+              } catch (e) {
+                // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+                return cachedSetTimeout.call(this, fun, 0);
+              }
+            }
+          }
+          function runClearTimeout(marker) {
+            if (cachedClearTimeout === clearTimeout) {
+              //normal enviroments in sane situations
+              return clearTimeout(marker);
+            }
+            // if clearTimeout wasn't available but was latter defined
+            if (
+              (cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) &&
+              clearTimeout
+            ) {
+              cachedClearTimeout = clearTimeout;
+              return clearTimeout(marker);
+            }
+            try {
+              // when when somebody has screwed with setTimeout but no I.E. maddness
+              return cachedClearTimeout(marker);
+            } catch (e) {
+              try {
+                // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+                return cachedClearTimeout.call(null, marker);
+              } catch (e) {
+                // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+                // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+                return cachedClearTimeout.call(this, marker);
+              }
+            }
+          }
+          var queue = [];
+          var draining = false;
+          var currentQueue;
+          var queueIndex = -1;
+
+          function cleanUpNextTick() {
+            if (!draining || !currentQueue) {
+              return;
+            }
+            draining = false;
+            if (currentQueue.length) {
+              queue = currentQueue.concat(queue);
+            } else {
+              queueIndex = -1;
+            }
+            if (queue.length) {
+              drainQueue();
+            }
+          }
+
+          function drainQueue() {
+            if (draining) {
+              return;
+            }
+            var timeout = runTimeout(cleanUpNextTick);
+            draining = true;
+
+            var len = queue.length;
+            while (len) {
+              currentQueue = queue;
+              queue = [];
+              while (++queueIndex < len) {
+                if (currentQueue) {
+                  currentQueue[queueIndex].run();
+                }
+              }
+              queueIndex = -1;
+              len = queue.length;
+            }
+            currentQueue = null;
+            draining = false;
+            runClearTimeout(timeout);
+          }
+
+          process.nextTick = function(fun) {
+            var args = new Array(arguments.length - 1);
+            if (arguments.length > 1) {
+              for (var i = 1; i < arguments.length; i++) {
+                args[i - 1] = arguments[i];
+              }
+            }
+            queue.push(new Item(fun, args));
+            if (queue.length === 1 && !draining) {
+              runTimeout(drainQueue);
+            }
+          };
+
+          // v8 likes predictible objects
+          function Item(fun, array) {
+            this.fun = fun;
+            this.array = array;
+          }
+          Item.prototype.run = function() {
+            this.fun.apply(null, this.array);
+          };
+          process.title = 'browser';
+          process.browser = true;
+          process.env = {};
+          process.argv = [];
+          process.version = ''; // empty string to avoid regexp issues
+          process.versions = {};
+
+          function noop() {}
+
+          process.on = noop;
+          process.addListener = noop;
+          process.once = noop;
+          process.off = noop;
+          process.removeListener = noop;
+          process.removeAllListeners = noop;
+          process.emit = noop;
+          process.prependListener = noop;
+          process.prependOnceListener = noop;
+
+          process.listeners = function(name) {
+            return [];
+          };
+
+          process.binding = function(name) {
+            throw new Error('process.binding is not supported');
+          };
+
+          process.cwd = function() {
+            return '/';
+          };
