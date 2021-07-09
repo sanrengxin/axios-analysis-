5680ccadb77716ba9d2720755099fcf1718abcfd
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-07-09 06:23:04 +0000

    update spring

--- p5.js
+++ p5.js
@@ -43731,3 +43731,137 @@
                 if (hue < 1) {
                   // Red to yellow (increasing green).
                   return zest + (val - zest) * hue;
+                } else if (hue < 3) {
+                  // Yellow to cyan (greatest green).
+                  return val;
+                } else if (hue < 4) {
+                  // Cyan to blue (decreasing green).
+                  return zest + (val - zest) * (4 - hue);
+                } else {
+                  // Blue to red (least green).
+                  return zest;
+                }
+              };
+
+              // Perform projections, offsetting hue as necessary.
+              RGBA = [
+                hzvToRGB(hue + 2, zest, val),
+                hzvToRGB(hue, zest, val),
+                hzvToRGB(hue - 2, zest, val),
+                hsla[3]
+              ];
+            }
+
+            return RGBA;
+          };
+
+          /**
+           * Convert an RGBA array to HSBA.
+           */
+          _main.default.ColorConversion._rgbaToHSBA = function(rgba) {
+            var red = rgba[0];
+            var green = rgba[1];
+            var blue = rgba[2];
+
+            var val = Math.max(red, green, blue);
+            var chroma = val - Math.min(red, green, blue);
+
+            var hue, sat;
+            if (chroma === 0) {
+              // Return early if grayscale.
+              hue = 0;
+              sat = 0;
+            } else {
+              sat = chroma / val;
+              if (red === val) {
+                // Magenta to yellow.
+                hue = (green - blue) / chroma;
+              } else if (green === val) {
+                // Yellow to cyan.
+                hue = 2 + (blue - red) / chroma;
+              } else if (blue === val) {
+                // Cyan to magenta.
+                hue = 4 + (red - green) / chroma;
+              }
+              if (hue < 0) {
+                // Confine hue to the interval [0, 1).
+                hue += 6;
+              } else if (hue >= 6) {
+                hue -= 6;
+              }
+            }
+
+            return [hue / 6, sat, val, rgba[3]];
+          };
+
+          /**
+           * Convert an RGBA array to HSLA.
+           */
+          _main.default.ColorConversion._rgbaToHSLA = function(rgba) {
+            var red = rgba[0];
+            var green = rgba[1];
+            var blue = rgba[2];
+
+            var val = Math.max(red, green, blue);
+            var min = Math.min(red, green, blue);
+            var li = val + min; // We will halve this later.
+            var chroma = val - min;
+
+            var hue, sat;
+            if (chroma === 0) {
+              // Return early if grayscale.
+              hue = 0;
+              sat = 0;
+            } else {
+              if (li < 1) {
+                sat = chroma / li;
+              } else {
+                sat = chroma / (2 - li);
+              }
+              if (red === val) {
+                // Magenta to yellow.
+                hue = (green - blue) / chroma;
+              } else if (green === val) {
+                // Yellow to cyan.
+                hue = 2 + (blue - red) / chroma;
+              } else if (blue === val) {
+                // Cyan to magenta.
+                hue = 4 + (red - green) / chroma;
+              }
+              if (hue < 0) {
+                // Confine hue to the interval [0, 1).
+                hue += 6;
+              } else if (hue >= 6) {
+                hue -= 6;
+              }
+            }
+
+            return [hue / 6, sat, li / 2, rgba[3]];
+          };
+          var _default = _main.default.ColorConversion;
+          exports.default = _default;
+        },
+        { '../core/main': 59 }
+      ],
+      45: [
+        function(_dereq_, module, exports) {
+          'use strict';
+          function _typeof(obj) {
+            if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
+              _typeof = function _typeof(obj) {
+                return typeof obj;
+              };
+            } else {
+              _typeof = function _typeof(obj) {
+                return obj &&
+                  typeof Symbol === 'function' &&
+                  obj.constructor === Symbol &&
+                  obj !== Symbol.prototype
+                  ? 'symbol'
+                  : typeof obj;
+              };
+            }
+            return _typeof(obj);
+          }
+          Object.defineProperty(exports, '__esModule', { value: true });
+          exports.default = void 0;
