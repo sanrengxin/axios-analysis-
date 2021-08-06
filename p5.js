4144230ddaf93cc2037f876741c24b8167ff272c
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-08-06 14:53:45 +0000

    tcp_cubic: fix the range of delayed_ack

--- p5.js
+++ p5.js
@@ -48064,3 +48064,138 @@
                 type: 'INVALIDTOKEN',
                 browser: 'Chrome'
               },
+
+              {
+                msg: "Unexpected token '{{.}}'",
+                type: 'UNEXPECTEDTOKEN',
+                browser: 'Chrome'
+              },
+
+              {
+                msg: "expected {{.}}, got '{{.}}'",
+                type: 'UNEXPECTEDTOKEN',
+                browser: 'Chrome'
+              }
+            ],
+
+            TypeError: [
+              {
+                msg: '{{.}} is not a function',
+                type: 'NOTFUNC',
+                browser: 'all'
+              }
+            ]
+          };
+          var _default = strings;
+          exports.default = _default;
+        },
+        {}
+      ],
+      51: [
+        function(_dereq_, module, exports) {
+          'use strict';
+          Object.defineProperty(exports, '__esModule', { value: true });
+          exports.default = void 0;
+
+          var _main = _interopRequireDefault(_dereq_('../main'));
+          var _internationalization = _dereq_('../internationalization');
+          function _interopRequireDefault(obj) {
+            return obj && obj.__esModule ? obj : { default: obj };
+          }
+          function _slicedToArray(arr, i) {
+            return (
+              _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest()
+            );
+          }
+          function _nonIterableRest() {
+            throw new TypeError('Invalid attempt to destructure non-iterable instance');
+          }
+          function _iterableToArrayLimit(arr, i) {
+            if (
+              !(
+                Symbol.iterator in Object(arr) ||
+                Object.prototype.toString.call(arr) === '[object Arguments]'
+              )
+            ) {
+              return;
+            }
+            var _arr = [];
+            var _n = true;
+            var _d = false;
+            var _e = undefined;
+            try {
+              for (
+                var _i = arr[Symbol.iterator](), _s;
+                !(_n = (_s = _i.next()).done);
+                _n = true
+              ) {
+                _arr.push(_s.value);
+                if (i && _arr.length === i) break;
+              }
+            } catch (err) {
+              _d = true;
+              _e = err;
+            } finally {
+              try {
+                if (!_n && _i['return'] != null) _i['return']();
+              } finally {
+                if (_d) throw _e;
+              }
+            }
+            return _arr;
+          }
+          function _arrayWithHoles(arr) {
+            if (Array.isArray(arr)) return arr;
+          }
+          function _typeof(obj) {
+            if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
+              _typeof = function _typeof(obj) {
+                return typeof obj;
+              };
+            } else {
+              _typeof = function _typeof(obj) {
+                return obj &&
+                  typeof Symbol === 'function' &&
+                  obj.constructor === Symbol &&
+                  obj !== Symbol.prototype
+                  ? 'symbol'
+                  : typeof obj;
+              };
+            }
+            return _typeof(obj);
+          }
+
+          // p5.js blue, p5.js orange, auto dark green; fallback p5.js darkened magenta
+          // See testColors below for all the color codes and names
+          var typeColors = ['#2D7BB6', '#EE9900', '#4DB200', '#C83C00'];
+          var misusedAtTopLevelCode = null;
+          var defineMisusedAtTopLevelCode = null;
+
+          // the threshold for the maximum allowed levenshtein distance
+          // used in misspelling detection
+          var EDIT_DIST_THRESHOLD = 2;
+
+          // to enable or disable styling (color, font-size, etc. ) for fes messages
+          var ENABLE_FES_STYLING = false;
+
+          if (typeof IS_MINIFIED !== 'undefined') {
+            _main.default._friendlyError = _main.default._checkForUserDefinedFunctions = _main.default._fesErrorMonitor = function() {};
+          } else {
+            var doFriendlyWelcome = false; // TEMP until we get it all working LM
+
+            var errorTable = _dereq_('./browser_errors').default;
+
+            // -- Borrowed from jQuery 1.11.3 --
+            var class2type = {};
+            var _toString = class2type.toString;
+            var names = [
+              'Boolean',
+              'Number',
+              'String',
+              'Function',
+              'Array',
+              'Date',
+              'RegExp',
+              'Object',
+              'Error'
+            ];
