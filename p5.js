57ba87f97235b42e06c6d61190af645cdb41fecf
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-09-12 20:37:36 +0000

    add extension

--- p5.js
+++ p5.js
@@ -52818,3 +52818,155 @@
            *   // with every loop through draw()
            *   // the graphics object on the right doesn't automatically reset
            *   // so translate() is additive and it moves down the screen
+           *   rect(0, 0, width / 2, 5);
+           *   pg.rect(0, 0, width / 2, 5);
+           *   translate(0, 5, 0);
+           *   pg.translate(0, 5, 0);
+           * }
+           * function mouseClicked() {
+           *   // if you click you will see that
+           *   // reset() resets the translate back to the initial state
+           *   // of the Graphics object
+           *   pg.reset();
+           * }
+           * </code></div>
+           *
+           * @alt
+           * A white line on a black background stays still on the top-left half.
+           * A black line animates from top to bottom on a white background on the right half.
+           * When clicked, the black line starts back over at the top.
+           */
+          _main.default.Graphics.prototype.reset = function() {
+            this._renderer.resetMatrix();
+            if (this._renderer.isP3D) {
+              this._renderer._update();
+            }
+          };
+
+          /**
+           * Removes a Graphics object from the page and frees any resources
+           * associated with it.
+           *
+           * @method remove
+           *
+           * @example
+           * <div class='norender'><code>
+           * let bg;
+           * function setup() {
+           *   bg = createCanvas(100, 100);
+           *   bg.background(0);
+           *   image(bg, 0, 0);
+           *   bg.remove();
+           * }
+           * </code></div>
+           *
+           * <div><code>
+           * let bg;
+           * function setup() {
+           *   pixelDensity(1);
+           *   createCanvas(100, 100);
+           *   stroke(255);
+           *   fill(0);
+           *
+           *   // create and draw the background image
+           *   bg = createGraphics(100, 100);
+           *   bg.background(200);
+           *   bg.ellipse(50, 50, 80, 80);
+           * }
+           * function draw() {
+           *   let t = millis() / 1000;
+           *   // draw the background
+           *   if (bg) {
+           *     image(bg, frameCount % 100, 0);
+           *     image(bg, frameCount % 100 - 100, 0);
+           *   }
+           *   // draw the foreground
+           *   let p = p5.Vector.fromAngle(t, 35).add(50, 50);
+           *   ellipse(p.x, p.y, 30);
+           * }
+           * function mouseClicked() {
+           *   // remove the background
+           *   if (bg) {
+           *     bg.remove();
+           *     bg = null;
+           *   }
+           * }
+           * </code></div>
+           *
+           * @alt
+           * no image
+           * a multi-colored circle moving back and forth over a scrolling background.
+           */
+          _main.default.Graphics.prototype.remove = function() {
+            if (this.elt.parentNode) {
+              this.elt.parentNode.removeChild(this.elt);
+            }
+            var idx = this._pInst._elements.indexOf(this);
+            if (idx !== -1) {
+              this._pInst._elements.splice(idx, 1);
+            }
+            for (var elt_ev in this._events) {
+              this.elt.removeEventListener(elt_ev, this._events[elt_ev]);
+            }
+          };
+          var _default = _main.default.Graphics;
+          exports.default = _default;
+        },
+        { './constants': 48, './main': 59 }
+      ],
+      62: [
+        function(_dereq_, module, exports) {
+          'use strict';
+          Object.defineProperty(exports, '__esModule', { value: true });
+          exports.default = void 0;
+
+          var _main = _interopRequireDefault(_dereq_('./main'));
+          var constants = _interopRequireWildcard(_dereq_('../core/constants'));
+          function _getRequireWildcardCache() {
+            if (typeof WeakMap !== 'function') return null;
+            var cache = new WeakMap();
+            _getRequireWildcardCache = function _getRequireWildcardCache() {
+              return cache;
+            };
+            return cache;
+          }
+          function _interopRequireWildcard(obj) {
+            if (obj && obj.__esModule) {
+              return obj;
+            }
+            if (obj === null || (_typeof(obj) !== 'object' && typeof obj !== 'function')) {
+              return { default: obj };
+            }
+            var cache = _getRequireWildcardCache();
+            if (cache && cache.has(obj)) {
+              return cache.get(obj);
+            }
+            var newObj = {};
+            var hasPropertyDescriptor =
+              Object.defineProperty && Object.getOwnPropertyDescriptor;
+            for (var key in obj) {
+              if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                var desc = hasPropertyDescriptor
+                  ? Object.getOwnPropertyDescriptor(obj, key)
+                  : null;
+                if (desc && (desc.get || desc.set)) {
+                  Object.defineProperty(newObj, key, desc);
+                } else {
+                  newObj[key] = obj[key];
+                }
+              }
+            }
+            newObj.default = obj;
+            if (cache) {
+              cache.set(obj, newObj);
+            }
+            return newObj;
+          }
+          function _interopRequireDefault(obj) {
+            return obj && obj.__esModule ? obj : { default: obj };
+          }
+          function _typeof(obj) {
+            if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
+              _typeof = function _typeof(obj) {
+                return typeof obj;
+              };
