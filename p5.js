7995d345aa09edbba6dd3645a4534606d231e5a1
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-09-13 07:52:23 +0000

    remove binding.pry

--- p5.js
+++ p5.js
@@ -52970,3 +52970,146 @@
               _typeof = function _typeof(obj) {
                 return typeof obj;
               };
+            } else {
+              _typeof = function _typeof(obj) {
+                return obj &&
+                  typeof Symbol === 'function' &&
+                  obj.constructor === Symbol &&
+                  obj !== Symbol.prototype
+                  ? 'symbol'
+                  : typeof obj;
+              };
+            }
+            return _typeof(obj);
+          }
+
+          /**
+           * Main graphics and rendering context, as well as the base API
+           * implementation for p5.js "core". To be used as the superclass for
+           * Renderer2D and Renderer3D classes, respectively.
+           *
+           * @class p5.Renderer
+           * @constructor
+           * @extends p5.Element
+           * @param {String} elt DOM node that is wrapped
+           * @param {p5} [pInst] pointer to p5 instance
+           * @param {Boolean} [isMainCanvas] whether we're using it as main canvas
+           */
+          _main.default.Renderer = function(elt, pInst, isMainCanvas) {
+            _main.default.Element.call(this, elt, pInst);
+            this.canvas = elt;
+            this._pixelsState = pInst;
+            if (isMainCanvas) {
+              this._isMainCanvas = true;
+              // for pixel method sharing with pimage
+              this._pInst._setProperty('_curElement', this);
+              this._pInst._setProperty('canvas', this.canvas);
+              this._pInst._setProperty('width', this.width);
+              this._pInst._setProperty('height', this.height);
+            } else {
+              // hide if offscreen buffer by default
+              this.canvas.style.display = 'none';
+              this._styles = []; // non-main elt styles stored in p5.Renderer
+            }
+
+            this._textSize = 12;
+            this._textLeading = 15;
+            this._textFont = 'sans-serif';
+            this._textStyle = constants.NORMAL;
+            this._textAscent = null;
+            this._textDescent = null;
+            this._textAlign = constants.LEFT;
+            this._textBaseline = constants.BASELINE;
+
+            this._rectMode = constants.CORNER;
+            this._ellipseMode = constants.CENTER;
+            this._curveTightness = 0;
+            this._imageMode = constants.CORNER;
+
+            this._tint = null;
+            this._doStroke = true;
+            this._doFill = true;
+            this._strokeSet = false;
+            this._fillSet = false;
+          };
+
+          _main.default.Renderer.prototype = Object.create(_main.default.Element.prototype);
+
+          // the renderer should return a 'style' object that it wishes to
+          // store on the push stack.
+          _main.default.Renderer.prototype.push = function() {
+            return {
+              properties: {
+                _doStroke: this._doStroke,
+                _strokeSet: this._strokeSet,
+                _doFill: this._doFill,
+                _fillSet: this._fillSet,
+                _tint: this._tint,
+                _imageMode: this._imageMode,
+                _rectMode: this._rectMode,
+                _ellipseMode: this._ellipseMode,
+                _textFont: this._textFont,
+                _textLeading: this._textLeading,
+                _textSize: this._textSize,
+                _textAlign: this._textAlign,
+                _textBaseline: this._textBaseline,
+                _textStyle: this._textStyle
+              }
+            };
+          };
+
+          // a pop() operation is in progress
+          // the renderer is passed the 'style' object that it returned
+          // from its push() method.
+          _main.default.Renderer.prototype.pop = function(style) {
+            if (style.properties) {
+              // copy the style properties back into the renderer
+              Object.assign(this, style.properties);
+            }
+          };
+
+          /**
+           * Resize our canvas element.
+           */
+          _main.default.Renderer.prototype.resize = function(w, h) {
+            this.width = w;
+            this.height = h;
+            this.elt.width = w * this._pInst._pixelDensity;
+            this.elt.height = h * this._pInst._pixelDensity;
+            this.elt.style.width = ''.concat(w, 'px');
+            this.elt.style.height = ''.concat(h, 'px');
+            if (this._isMainCanvas) {
+              this._pInst._setProperty('width', this.width);
+              this._pInst._setProperty('height', this.height);
+            }
+          };
+
+          _main.default.Renderer.prototype.get = function(x, y, w, h) {
+            var pixelsState = this._pixelsState;
+            var pd = pixelsState._pixelDensity;
+            var canvas = this.canvas;
+
+            if (typeof x === 'undefined' && typeof y === 'undefined') {
+              // get()
+              x = y = 0;
+              w = pixelsState.width;
+              h = pixelsState.height;
+            } else {
+              x *= pd;
+              y *= pd;
+
+              if (typeof w === 'undefined' && typeof h === 'undefined') {
+                // get(x,y)
+                if (x < 0 || y < 0 || x >= canvas.width || y >= canvas.height) {
+                  return [0, 0, 0, 0];
+                }
+
+                return this._getPixel(x, y);
+              }
+              // get(x,y,w,h)
+            }
+
+            var region = new _main.default.Image(w, h);
+            region.canvas
+              .getContext('2d')
+              .drawImage(canvas, x, y, w * pd, h * pd, 0, 0, w, h);
