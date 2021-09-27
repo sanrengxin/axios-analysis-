f029cf2323d78838202931d6a259e733d9e0a5cb
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-09-27 08:20:04 +0000

    add constructor

--- p5.js
+++ p5.js
@@ -55263,3 +55263,160 @@
            *
            * function draw() {
            *   background(200);
+           *   pg.background(100);
+           *   pg.noStroke();
+           *   pg.ellipse(pg.width / 2, pg.height / 2, 50, 50);
+           *   image(pg, 50, 50);
+           *   image(pg, 0, 0, 50, 50);
+           * }
+           * </code>
+           * </div>
+           *
+           * @alt
+           * 4 grey squares alternating light and dark grey. White quarter circle mid-left.
+           */
+          _main.default.prototype.createGraphics = function(w, h, renderer) {
+            _main.default._validateParameters('createGraphics', arguments);
+            return new _main.default.Graphics(w, h, renderer, this);
+          };
+
+          /**
+           * Blends the pixels in the display window according to the defined mode.
+           * There is a choice of the following modes to blend the source pixels (A)
+           * with the ones of pixels already in the display window (B):
+           * <ul>
+           * <li><code>BLEND</code> - linear interpolation of colours: C =
+           * A\*factor + B. <b>This is the default blending mode.</b></li>
+           * <li><code>ADD</code> - sum of A and B</li>
+           * <li><code>DARKEST</code> - only the darkest colour succeeds: C =
+           * min(A\*factor, B).</li>
+           * <li><code>LIGHTEST</code> - only the lightest colour succeeds: C =
+           * max(A\*factor, B).</li>
+           * <li><code>DIFFERENCE</code> - subtract colors from underlying image.</li>
+           * <li><code>EXCLUSION</code> - similar to <code>DIFFERENCE</code>, but less
+           * extreme.</li>
+           * <li><code>MULTIPLY</code> - multiply the colors, result will always be
+           * darker.</li>
+           * <li><code>SCREEN</code> - opposite multiply, uses inverse values of the
+           * colors.</li>
+           * <li><code>REPLACE</code> - the pixels entirely replace the others and
+           * don't utilize alpha (transparency) values.</li>
+           * <li><code>REMOVE</code> - removes pixels from B with the alpha strength of A.</li>
+           * <li><code>OVERLAY</code> - mix of <code>MULTIPLY</code> and <code>SCREEN
+           * </code>. Multiplies dark values, and screens light values. <em>(2D)</em></li>
+           * <li><code>HARD_LIGHT</code> - <code>SCREEN</code> when greater than 50%
+           * gray, <code>MULTIPLY</code> when lower. <em>(2D)</em></li>
+           * <li><code>SOFT_LIGHT</code> - mix of <code>DARKEST</code> and
+           * <code>LIGHTEST</code>. Works like <code>OVERLAY</code>, but not as harsh. <em>(2D)</em>
+           * </li>
+           * <li><code>DODGE</code> - lightens light tones and increases contrast,
+           * ignores darks. <em>(2D)</em></li>
+           * <li><code>BURN</code> - darker areas are applied, increasing contrast,
+           * ignores lights. <em>(2D)</em></li>
+           * <li><code>SUBTRACT</code> - remainder of A and B <em>(3D)</em></li>
+           * </ul>
+           *
+           * <em>(2D)</em> indicates that this blend mode <b>only</b> works in the 2D renderer.<br>
+           * <em>(3D)</em> indicates that this blend mode <b>only</b> works in the WEBGL renderer.
+           *
+           * @method blendMode
+           * @param  {Constant} mode blend mode to set for canvas.
+           *                either BLEND, DARKEST, LIGHTEST, DIFFERENCE, MULTIPLY,
+           *                EXCLUSION, SCREEN, REPLACE, OVERLAY, HARD_LIGHT,
+           *                SOFT_LIGHT, DODGE, BURN, ADD, REMOVE or SUBTRACT
+           * @example
+           * <div>
+           * <code>
+           * blendMode(LIGHTEST);
+           * strokeWeight(30);
+           * stroke(80, 150, 255);
+           * line(25, 25, 75, 75);
+           * stroke(255, 50, 50);
+           * line(75, 25, 25, 75);
+           * </code>
+           * </div>
+           *
+           * <div>
+           * <code>
+           * blendMode(MULTIPLY);
+           * strokeWeight(30);
+           * stroke(80, 150, 255);
+           * line(25, 25, 75, 75);
+           * stroke(255, 50, 50);
+           * line(75, 25, 25, 75);
+           * </code>
+           * </div>
+           *
+           * @alt
+           * translucent image thick red & blue diagonal rounded lines intersecting center
+           * Thick red & blue diagonal rounded lines intersecting center. dark at overlap
+           */
+          _main.default.prototype.blendMode = function(mode) {
+            _main.default._validateParameters('blendMode', arguments);
+            if (mode === constants.NORMAL) {
+              // Warning added 3/26/19, can be deleted in future (1.0 release?)
+              console.warn(
+                'NORMAL has been deprecated for use in blendMode. defaulting to BLEND instead.'
+              );
+
+              mode = constants.BLEND;
+            }
+            this._renderer.blendMode(mode);
+          };
+
+          /**
+           * The p5.js API provides a lot of functionality for creating graphics, but there is
+           * some native HTML5 Canvas functionality that is not exposed by p5. You can still call
+           * it directly using the variable `drawingContext`, as in the example shown. This is
+           * the equivalent of calling `canvas.getContext('2d');` or `canvas.getContext('webgl');`.
+           * See this
+           * <a href="https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D">
+           * reference for the native canvas API</a> for possible drawing functions you can call.
+           *
+           * @property drawingContext
+           * @example
+           * <div>
+           * <code>
+           * function setup() {
+           *   drawingContext.shadowOffsetX = 5;
+           *   drawingContext.shadowOffsetY = -5;
+           *   drawingContext.shadowBlur = 10;
+           *   drawingContext.shadowColor = 'black';
+           *   background(200);
+           *   ellipse(width / 2, height / 2, 50, 50);
+           * }
+           * </code>
+           * </div>
+           *
+           * @alt
+           * white ellipse with shadow blur effect around edges
+           */ var _default = _main.default;
+          exports.default = _default;
+        },
+        {
+          '../webgl/p5.RendererGL': 113,
+          './constants': 48,
+          './main': 59,
+          './p5.Graphics': 61,
+          './p5.Renderer2D': 63
+        }
+      ],
+      66: [
+        function(_dereq_, module, exports) {
+          'use strict';
+          function _typeof(obj) {
+            if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
+              _typeof = function _typeof(obj) {
+                return typeof obj;
+              };
+            } else {
+              _typeof = function _typeof(obj) {
+                return obj &&
+                  typeof Symbol === 'function' &&
+                  obj.constructor === Symbol &&
+                  obj !== Symbol.prototype
+                  ? 'symbol'
+                  : typeof obj;
+              };
+            }
+            return _typeof(obj);
