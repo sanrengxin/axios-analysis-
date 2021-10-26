7f02f344f18a2e559aebe044d036915190093e74
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2021-10-26 20:39:20 +0000

    Fixe

--- p5.js
+++ p5.js
@@ -59611,3 +59611,140 @@
            *
            * function draw() {
            *   textSize(40);
+           *   background(255);
+           *   text(myText, width / 2, height / 2);
+           * }
+           *
+           * function keyPressed() {
+           *   myText = key;
+           *   storeItem('myText', myText);
+           * }
+           * </code></div>
+           *
+           * @alt
+           * When you type the key name is displayed as black text on white background.
+           * If you reload the page, the last letter typed is still displaying.
+           */
+          _main.default.prototype.storeItem = function(key, value) {
+            if (typeof key !== 'string') {
+              console.log(
+                'The argument that you passed to storeItem() - '.concat(
+                  key,
+                  ' is not a string.'
+                )
+              );
+            }
+            if (key.endsWith('p5TypeID')) {
+              console.log(
+                'The argument that you passed to storeItem() - '.concat(
+                  key,
+                  " must not end with 'p5TypeID'."
+                )
+              );
+            }
+
+            if (typeof value === 'undefined') {
+              console.log('You cannot store undefined variables using storeItem().');
+            }
+            var type = _typeof(value);
+            switch (type) {
+              case 'number':
+              case 'boolean':
+                value = value.toString();
+                break;
+              case 'object':
+                if (value instanceof _main.default.Color) {
+                  type = 'p5.Color';
+                } else if (value instanceof _main.default.Vector) {
+                  type = 'p5.Vector';
+                  var coord = [value.x, value.y, value.z];
+                  value = coord;
+                }
+                value = JSON.stringify(value);
+                break;
+              case 'string':
+              default:
+                break;
+            }
+
+            localStorage.setItem(key, value);
+            var typeKey = ''.concat(key, 'p5TypeID');
+            localStorage.setItem(typeKey, type);
+          };
+
+          /**
+           *
+           * Returns the value of an item that was stored in local storage
+           * using storeItem()
+           *
+           * @method getItem
+           * @for p5
+           * @param {String} key name that you wish to use to store in local storage
+           * @return {Number|Object|String|Boolean|p5.Color|p5.Vector} Value of stored item
+           *
+           * @example
+           * <div><code>
+           * // Click the mouse to change
+           * // the color of the background
+           * // Once you have changed the color
+           * // it will stay changed even when you
+           * // reload the page.
+           *
+           * let myColor;
+           *
+           * function setup() {
+           *   createCanvas(100, 100);
+           *   myColor = getItem('myColor');
+           * }
+           *
+           * function draw() {
+           *   if (myColor !== null) {
+           *     background(myColor);
+           *   }
+           * }
+           *
+           * function mousePressed() {
+           *   myColor = color(random(255), random(255), random(255));
+           *   storeItem('myColor', myColor);
+           * }
+           * </code></div>
+           *
+           * @alt
+           * If you click, the canvas changes to a random color.
+           * If you reload the page, the canvas is still the color it
+           * was when the page was previously loaded.
+           */
+          _main.default.prototype.getItem = function(key) {
+            var value = localStorage.getItem(key);
+            var type = localStorage.getItem(''.concat(key, 'p5TypeID'));
+            if (typeof type === 'undefined') {
+              console.log(
+                'Unable to determine type of item stored under '.concat(
+                  key,
+                  'in local storage. Did you save the item with something other than setItem()?'
+                )
+              );
+            } else if (value !== null) {
+              switch (type) {
+                case 'number':
+                  value = parseFloat(value);
+                  break;
+                case 'boolean':
+                  value = value === 'true';
+                  break;
+                case 'object':
+                  value = JSON.parse(value);
+                  break;
+                case 'p5.Color':
+                  value = JSON.parse(value);
+                  value = this.color.apply(this, _toConsumableArray(value.levels));
+                  break;
+                case 'p5.Vector':
+                  value = JSON.parse(value);
+                  value = this.createVector.apply(this, _toConsumableArray(value));
+                  break;
+                case 'string':
+                default:
+                  break;
+              }
+            }
