7e551d844523a253610c146c979784ca4290e3bd
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2022-01-13 09:52:11 +0000

    Bump version to 1.5.2

--- p5.js
+++ p5.js
@@ -65274,3 +65274,130 @@
            * The <a href="#/p5/keyPressed">keyPressed()</a> function is called once every time a key is pressed. The
            * keyCode for the key that was pressed is stored in the <a href="#/p5/keyCode">keyCode</a> variable.
            *
+           * For non-ASCII keys, use the keyCode variable. You can check if the keyCode
+           * equals BACKSPACE, DELETE, ENTER, RETURN, TAB, ESCAPE, SHIFT, CONTROL,
+           * OPTION, ALT, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW.
+           *
+           * For ASCII keys, the key that was pressed is stored in the key variable. However, it
+           * does not distinguish between uppercase and lowercase. For this reason, it
+           * is recommended to use <a href="#/p5/keyTyped">keyTyped()</a> to read the key variable, in which the
+           * case of the variable will be distinguished.
+           *
+           * Because of how operating systems handle key repeats, holding down a key
+           * may cause multiple calls to <a href="#/p5/keyTyped">keyTyped()</a> (and <a href="#/p5/keyReleased">keyReleased()</a> as well). The
+           * rate of repeat is set by the operating system and how each computer is
+           * configured.<br><br>
+           * Browsers may have different default
+           * behaviors attached to various key events. To prevent any default
+           * behavior for this event, add "return false" to the end of the method.
+           *
+           * @method keyPressed
+           * @example
+           * <div>
+           * <code>
+           * let value = 0;
+           * function draw() {
+           *   fill(value);
+           *   rect(25, 25, 50, 50);
+           * }
+           * function keyPressed() {
+           *   if (value === 0) {
+           *     value = 255;
+           *   } else {
+           *     value = 0;
+           *   }
+           * }
+           * </code>
+           * </div>
+           * <div>
+           * <code>
+           * let value = 0;
+           * function draw() {
+           *   fill(value);
+           *   rect(25, 25, 50, 50);
+           * }
+           * function keyPressed() {
+           *   if (keyCode === LEFT_ARROW) {
+           *     value = 255;
+           *   } else if (keyCode === RIGHT_ARROW) {
+           *     value = 0;
+           *   }
+           * }
+           * </code>
+           * </div>
+           * <div class="norender">
+           * <code>
+           * function keyPressed() {
+           *   // Do something
+           *   return false; // prevent any default behaviour
+           * }
+           * </code>
+           * </div>
+           *
+           * @alt
+           * black rect center. turns white when key pressed and black when released
+           * black rect center. turns white when left arrow pressed and black when right.
+           */
+          _main.default.prototype._onkeydown = function(e) {
+            if (this._downKeys[e.which]) {
+              // prevent multiple firings
+              return;
+            }
+            this._setProperty('isKeyPressed', true);
+            this._setProperty('keyIsPressed', true);
+            this._setProperty('keyCode', e.which);
+            this._downKeys[e.which] = true;
+            this._setProperty('key', e.key || String.fromCharCode(e.which) || e.which);
+            var context = this._isGlobal ? window : this;
+            if (typeof context.keyPressed === 'function' && !e.charCode) {
+              var executeDefault = context.keyPressed(e);
+              if (executeDefault === false) {
+                e.preventDefault();
+              }
+            }
+          };
+          /**
+           * The <a href="#/p5/keyReleased">keyReleased()</a> function is called once every time a key is released.
+           * See <a href="#/p5/key">key</a> and <a href="#/p5/keyCode">keyCode</a> for more information.<br><br>
+           * Browsers may have different default
+           * behaviors attached to various key events. To prevent any default
+           * behavior for this event, add "return false" to the end of the method.
+           *
+           * @method keyReleased
+           * @example
+           * <div>
+           * <code>
+           * let value = 0;
+           * function draw() {
+           *   fill(value);
+           *   rect(25, 25, 50, 50);
+           * }
+           * function keyReleased() {
+           *   if (value === 0) {
+           *     value = 255;
+           *   } else {
+           *     value = 0;
+           *   }
+           *   return false; // prevent any default behavior
+           * }
+           * </code>
+           * </div>
+           *
+           * @alt
+           * black rect center. turns white when key pressed and black when pressed again
+           */
+          _main.default.prototype._onkeyup = function(e) {
+            this._downKeys[e.which] = false;
+
+            if (!this._areDownKeys()) {
+              this._setProperty('isKeyPressed', false);
+              this._setProperty('keyIsPressed', false);
+            }
+
+            this._setProperty('_lastKeyCodeTyped', null);
+
+            this._setProperty('key', e.key || String.fromCharCode(e.which) || e.which);
+            this._setProperty('keyCode', e.which);
+
+            var context = this._isGlobal ? window : this;
+            if (typeof context.keyReleased === 'function') {
