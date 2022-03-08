cf9ef41ea275162d4b06c80b5976315f6d6ac32a
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2022-03-08 20:17:13 +0000

    more work on tests

--- p5.js
+++ p5.js
@@ -68485,3 +68485,144 @@
               } else if (loopLimit === 0) {
                 loopLimit = null;
               }
+
+              // we used the pImg for painting and saving during load
+              // so we have to reset it to the first frame
+              pImg.drawingContext.putImageData(frames[0].image, 0, 0);
+
+              pImg.gifProperties = {
+                displayIndex: 0,
+                loopLimit: loopLimit,
+                loopCount: 0,
+                frames: frames,
+                numFrames: numFrames,
+                playing: true,
+                timeDisplayed: 0,
+                lastChangeTime: 0
+              };
+            }
+
+            if (typeof successCallback === 'function') {
+              successCallback(pImg);
+            }
+            finishCallback();
+          }
+
+          /**
+           * Validates clipping params. Per drawImage spec sWidth and sHight cannot be
+           * negative or greater than image intrinsic width and height
+           * @private
+           * @param {Number} sVal
+           * @param {Number} iVal
+           * @returns {Number}
+           * @private
+           */
+          function _sAssign(sVal, iVal) {
+            if (sVal > 0 && sVal < iVal) {
+              return sVal;
+            } else {
+              return iVal;
+            }
+          }
+
+          /**
+           * Draw an image to the p5.js canvas.
+           *
+           * This function can be used with different numbers of parameters. The
+           * simplest use requires only three parameters: img, x, and y—where (x, y) is
+           * the position of the image. Two more parameters can optionally be added to
+           * specify the width and height of the image.
+           *
+           * This function can also be used with all eight Number parameters. To
+           * differentiate between all these parameters, p5.js uses the language of
+           * "destination rectangle" (which corresponds to "dx", "dy", etc.) and "source
+           * image" (which corresponds to "sx", "sy", etc.) below. Specifying the
+           * "source image" dimensions can be useful when you want to display a
+           * subsection of the source image instead of the whole thing. Here's a diagram
+           * to explain further:
+           * <img src="assets/drawImage.png"></img>
+           *
+           * @method image
+           * @param  {p5.Image|p5.Element} img    the image to display
+           * @param  {Number}   x     the x-coordinate of the top-left corner of the image
+           * @param  {Number}   y     the y-coordinate of the top-left corner of the image
+           * @param  {Number}   [width]  the width to draw the image
+           * @param  {Number}   [height] the height to draw the image
+           * @example
+           * <div>
+           * <code>
+           * let img;
+           * function preload() {
+           *   img = loadImage('assets/laDefense.jpg');
+           * }
+           * function setup() {
+           *   // Top-left corner of the img is at (0, 0)
+           *   // Width and height are the img's original width and height
+           *   image(img, 0, 0);
+           * }
+           * </code>
+           * </div>
+           * <div>
+           * <code>
+           * let img;
+           * function preload() {
+           *   img = loadImage('assets/laDefense.jpg');
+           * }
+           * function setup() {
+           *   background(50);
+           *   // Top-left corner of the img is at (10, 10)
+           *   // Width and height are 50 x 50
+           *   image(img, 10, 10, 50, 50);
+           * }
+           * </code>
+           * </div>
+           * <div>
+           * <code>
+           * function setup() {
+           *   // Here, we use a callback to display the image after loading
+           *   loadImage('assets/laDefense.jpg', img => {
+           *     image(img, 0, 0);
+           *   });
+           * }
+           * </code>
+           * </div>
+           * <div>
+           * <code>
+           * let img;
+           * function preload() {
+           *   img = loadImage('assets/gradient.png');
+           * }
+           * function setup() {
+           *   // 1. Background image
+           *   // Top-left corner of the img is at (0, 0)
+           *   // Width and height are the img's original width and height, 100 x 100
+           *   image(img, 0, 0);
+           *   // 2. Top right image
+           *   // Top-left corner of destination rectangle is at (50, 0)
+           *   // Destination rectangle width and height are 40 x 20
+           *   // The next parameters are relative to the source image:
+           *   // - Starting at position (50, 50) on the source image, capture a 50 x 50
+           *   // subsection
+           *   // - Draw this subsection to fill the dimensions of the destination rectangle
+           *   image(img, 50, 0, 40, 20, 50, 50, 50, 50);
+           * }
+           * </code>
+           * </div>
+           * @alt
+           * image of the underside of a white umbrella and gridded ceiling above
+           * image of the underside of a white umbrella and gridded ceiling above
+           */
+          /**
+           * @method image
+           * @param  {p5.Image|p5.Element} img
+           * @param  {Number}   dx     the x-coordinate of the destination
+           *                           rectangle in which to draw the source image
+           * @param  {Number}   dy     the y-coordinate of the destination
+           *                           rectangle in which to draw the source image
+           * @param  {Number}   dWidth  the width of the destination rectangle
+           * @param  {Number}   dHeight the height of the destination rectangle
+           * @param  {Number}   sx     the x-coordinate of the subsection of the source
+           * image to draw into the destination rectangle
+           * @param  {Number}   sy     the y-coordinate of the subsection of the source
+           * image to draw into the destination rectangle
+           * @param {Number}    [sWidth] the width of the subsection of the
