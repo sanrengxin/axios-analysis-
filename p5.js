d8b5336d5376e48605d7a63852455e13481c122c
sanrengxin <100178448+sanrengxin@users.noreply.github.com>
2022-05-16 18:26:05 +0000

    Create .babelrc

--- p5.js
+++ p5.js
@@ -76637,3 +76637,131 @@
            *   }
            * }
            * </code>
+           * </div>
+           *
+           * @alt
+           * vertical line moves left to right with updating noise values.
+           * horizontal wave pattern effected by mouse x-position & updating noise values.
+           */ _main.default.prototype.noise = function(x) {
+            var y = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+            var z = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0;
+            if (perlin == null) {
+              perlin = new Array(PERLIN_SIZE + 1);
+              for (var i = 0; i < PERLIN_SIZE + 1; i++) {
+                perlin[i] = Math.random();
+              }
+            }
+
+            if (x < 0) {
+              x = -x;
+            }
+            if (y < 0) {
+              y = -y;
+            }
+            if (z < 0) {
+              z = -z;
+            }
+
+            var xi = Math.floor(x),
+              yi = Math.floor(y),
+              zi = Math.floor(z);
+            var xf = x - xi;
+            var yf = y - yi;
+            var zf = z - zi;
+            var rxf, ryf;
+
+            var r = 0;
+            var ampl = 0.5;
+
+            var n1, n2, n3;
+
+            for (var o = 0; o < perlin_octaves; o++) {
+              var of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+
+              rxf = scaled_cosine(xf);
+              ryf = scaled_cosine(yf);
+
+              n1 = perlin[of & PERLIN_SIZE];
+              n1 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n1);
+              n2 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+              n2 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+              n1 += ryf * (n2 - n1);
+
+              of += PERLIN_ZWRAP;
+              n2 = perlin[of & PERLIN_SIZE];
+              n2 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n2);
+              n3 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+              n3 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+              n2 += ryf * (n3 - n2);
+
+              n1 += scaled_cosine(zf) * (n2 - n1);
+
+              r += n1 * ampl;
+              ampl *= perlin_amp_falloff;
+              xi <<= 1;
+              xf *= 2;
+              yi <<= 1;
+              yf *= 2;
+              zi <<= 1;
+              zf *= 2;
+
+              if (xf >= 1.0) {
+                xi++;
+                xf--;
+              }
+              if (yf >= 1.0) {
+                yi++;
+                yf--;
+              }
+              if (zf >= 1.0) {
+                zi++;
+                zf--;
+              }
+            }
+            return r;
+          };
+
+          /**
+           *
+           * Adjusts the character and level of detail produced by the Perlin noise
+           * function. Similar to harmonics in physics, noise is computed over
+           * several octaves. Lower octaves contribute more to the output signal and
+           * as such define the overall intensity of the noise, whereas higher octaves
+           * create finer grained details in the noise sequence.
+           *
+           * By default, noise is computed over 4 octaves with each octave contributing
+           * exactly half than its predecessor, starting at 50% strength for the 1st
+           * octave. This falloff amount can be changed by adding an additional function
+           * parameter. Eg. a falloff factor of 0.75 means each octave will now have
+           * 75% impact (25% less) of the previous lower octave. Any value between
+           * 0.0 and 1.0 is valid, however note that values greater than 0.5 might
+           * result in greater than 1.0 values returned by <b>noise()</b>.
+           *
+           * By changing these parameters, the signal created by the <b>noise()</b>
+           * function can be adapted to fit very specific needs and characteristics.
+           *
+           * @method noiseDetail
+           * @param {Number} lod number of octaves to be used by the noise
+           * @param {Number} falloff falloff factor for each octave
+           * @example
+           * <div>
+           * <code>
+           * let noiseVal;
+           * let noiseScale = 0.02;
+           *
+           * function setup() {
+           *   createCanvas(100, 100);
+           * }
+           *
+           * function draw() {
+           *   background(0);
+           *   for (let y = 0; y < height; y++) {
+           *     for (let x = 0; x < width / 2; x++) {
+           *       noiseDetail(2, 0.2);
+           *       noiseVal = noise((mouseX + x) * noiseScale, (mouseY + y) * noiseScale);
+           *       stroke(noiseVal * 255);
+           *       point(x, y);
+           *       noiseDetail(8, 0.65);
+           *       noiseVal = noise(
+           *         (mouseX + x + width / 2) * noiseScale,
+           *         (mouseY + y) * noiseScale
